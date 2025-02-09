bc2260aa2ffa83254a9b31e0c6c52736419894a0
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-02-09 11:40:00 +0000

    Added tuner.

---
+++ tools/tuner/tuner.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"math"
+	"math/rand/v2"
+	"os"
+	"runtime/pprof"
+	"strconv"
+	"strings"
+
+	"github.com/paulsonkoly/chess-3/board"
+	"github.com/paulsonkoly/chess-3/eval"
+	"github.com/paulsonkoly/chess-3/tools/tuner/tuning"
+	"github.com/paulsonkoly/chess-3/types"
+)
+
+var epdF = flag.String("epd", "", "epd file name")
+var cpuProf = flag.String("cpuProf", "", "cpu profile file name")
+
+// var memProf = flag.String("memProf", "", "mem profile file name")
+
+type EPDEntry struct {
+	b *board.Board
+	r float64
+}
+
+const BatchSize = 100_000
+
+func main() {
+	flag.Parse()
+
+	if *cpuProf != "" {
+		cpu, err := os.Create(*cpuProf)
+		if err != nil {
+			panic(err)
+		}
+		err = pprof.StartCPUProfile(cpu)
+		if err != nil {
+			panic(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	if *epdF == "" {
+		panic("epd file name is required")
+	}
+
+	// read in the data set
+	data := make([]EPDEntry, 0)
+	f, err := os.Open(*epdF)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	scn := bufio.NewScanner(f)
+	cnt := 0
+	fmt.Println("Reading data...")
+	for scn.Scan() {
+		line := scn.Text()
+
+		splits := strings.Split(line, "; ")
+
+		if len(splits) != 2 {
+			panic("epd line error " + line)
+		}
+
+		b := board.FromFEN(splits[0])
+		r, err := strconv.ParseFloat(splits[1], 64)
+		if err != nil {
+			panic(err)
+		}
+
+		data = append(data, EPDEntry{b, r})
+		cnt++
+	}
+	fmt.Printf("%d lines read\n", cnt)
+
+	fmt.Println("initial coefficients:")
+	coeffs := tuning.InitialCoeffs()
+	coeffs.Print()
+
+	k := 0.832 // a scaling constant
+
+	// minimize for k
+	improved := true
+	step := 1.0
+	bestE := computeE(data, k, coeffs)
+	for step > 0.0001 {
+		fmt.Println("step", step)
+		for improved {
+			eHigh := computeE(data, k+step, coeffs)
+			eLow := computeE(data, k-step, coeffs)
+			improved = false
+
+			nK := k - step
+			nE := eLow
+			if eHigh < eLow {
+				nK = k + step
+				nE = eHigh
+			}
+
+			if nE < bestE {
+				improved = true
+				bestE = nE
+				k = nK
+				fmt.Println("new k value: ", k)
+			}
+		}
+		step /= 10.0
+		improved = true
+	}
+
+	fmt.Println("bestE ", bestE)
+
+	momentum := tuning.Coeffs{}
+	velocity := tuning.Coeffs{}
+
+	beta1 := 0.9
+	beta2 := 0.999
+	learningRate := 0.1
+	epsilon := 0.001
+
+	bStart := 0
+	for epoch := 1; true; { // epochs
+		batch := data[bStart:min(bStart+BatchSize, len(data))]
+
+		// calculate the gradients
+		grad := tuning.Coeffs{}
+		for _, e := range batch {
+			score := evalCoeffs(e.b, coeffs)
+			sigm := sigmoid(score, k)
+			loss := (e.r - sigm) * (e.r - sigm)
+			for ixs := range grad.Loop() {
+				c := coeffs.At(ixs)
+				o := *c
+
+				*c += epsilon
+				score2 := evalCoeffs(e.b, coeffs)
+				*c = o
+
+				sigm2 := sigmoid(score2, k)
+				loss2 := (e.r - sigm2) * (e.r - sigm2)
+
+				g := (loss2 - loss) / epsilon
+
+				*grad.At(ixs) += g
+			}
+		}
+
+		for ixs := range coeffs.Loop() {
+			g := *grad.At(ixs) / float64(len(batch))
+			m := momentum.At(ixs)
+			*m = beta1*(*m) + (1-beta1)*g
+			v := velocity.At(ixs)
+			*v = beta2*(*v) + (1-beta2)*math.Pow(g, 2)
+
+			mHat := *m / (1 - math.Pow(beta1, float64(epoch)))
+			vHat := *v / (1 - math.Pow(beta2, float64(epoch)))
+
+			c := coeffs.At(ixs)
+			*c -= learningRate * mHat / (1e-8 + math.Sqrt(vHat))
+		}
+
+		bStart += BatchSize
+		if bStart >= len(data) {
+			fmt.Printf("epoch %d finished\n", epoch)
+
+			bestE = computeE(data, k, coeffs)
+			fmt.Println("bestE ", bestE)
+
+			coeffs.Print()
+
+			rand.Shuffle(len(data), func(i, j int) {
+				data[i], data[j] = data[j], data[i]
+			})
+
+			epoch++
+			bStart = 0
+		}
+	}
+}
+
+func computeE(data []EPDEntry, k float64, coeffs *tuning.Coeffs) float64 {
+	sum := 0.0
+	count := 0
+
+	for _, epdE := range data {
+		b := epdE.b
+		r := epdE.r
+
+		score := evalCoeffs(b, coeffs)
+
+		sgm := 1 / (1 + math.Exp(-k*float64(score)/400))
+
+		sum += (r - sgm) * (r - sgm)
+
+		count++
+	}
+
+	return sum
+}
+
+func evalCoeffs(b *board.Board, coeffs *tuning.Coeffs) float64 {
+	score := eval.Eval(b, -10_001.0, 10_001.0, coeffs.ToEvalType())
+
+	if b.STM == types.Black {
+		score = -score
+	}
+	return score
+}
+
+func sigmoid(v, k float64) float64 {
+	return 1 / (1 + math.Exp(-k*v/400))
+}
---
+++ tools/tuner/tuning/tuning.go
@@ -0,0 +1,204 @@
+package tuning
+
+import (
+	"fmt"
+	"iter"
+	"math"
+	"reflect"
+	"slices"
+	"strings"
+
+	"github.com/paulsonkoly/chess-3/eval"
+)
+
+// Targets controls which fields in eval.CoeffSet are going to be tuned.
+var Targets = [...]string{
+	"TempoBonus",
+	"MobilityKnight", "MobilityBishop", "MobilityRook",
+	"KingAttackPieces", "KingAttackSquares",
+}
+
+type Coeffs eval.CoeffSet[float64]
+
+// InitialCoeffs creates the initial coefficents by converting eval.Coefficients from Score to Tunable.
+func InitialCoeffs() *Coeffs {
+	result := Coeffs{}
+
+	t := reflect.TypeOf(result)
+	dst := reflect.ValueOf(&result).Elem()
+	src := reflect.ValueOf(&eval.Coefficients).Elem()
+
+	for ix := 0; ix < t.NumField(); ix++ {
+		convert(dst.Field(ix), src.Field(ix))
+	}
+
+	return &result
+}
+
+func (t *Coeffs) ToEvalType() *eval.CoeffSet[float64] {
+	return (*eval.CoeffSet[float64])(t)
+}
+
+func convert(dst, src reflect.Value) {
+	switch dst.Kind() {
+
+	case reflect.Array:
+		if src.Kind() != reflect.Array {
+			panic("array expected")
+		}
+		if src.Len() != dst.Len() {
+			panic("len mismatch")
+		}
+		for i := 0; i < dst.Len(); i++ {
+			convert(dst.Index(i), src.Index(i))
+		}
+
+	case reflect.Float64:
+		if src.Kind() != reflect.Int16 {
+			panic("int16 expected")
+		}
+		dst.Set(reflect.ValueOf(float64(src.Int())))
+
+	default:
+		panic("unexpected kind " + dst.Kind().String())
+	}
+}
+
+func (t *Coeffs) Print() {
+	typ := reflect.TypeOf(*t)
+	for ix := 0; ix < typ.NumField(); ix++ {
+		f := typ.Field(ix)
+		v := reflect.ValueOf(*t).Field(ix)
+
+		fmt.Printf("%s: ", f.Name)
+
+		printField(v, 0)
+		fmt.Printf(",\n")
+	}
+}
+
+func indent(i int) string {
+	return strings.Repeat("\t", i)
+}
+
+func printField(v reflect.Value, in int) {
+	switch v.Kind() {
+
+	case reflect.Array:
+
+		switch {
+
+		case v.Len() == 64: // print 8x8 format
+			fmt.Print("{\n")
+			newLine := ""
+			for i := range 8 {
+				fmt.Printf("%s%s", newLine, indent(in+1))
+				comma := ""
+				for j := range 8 {
+					e := v.Index(i*8 + j)
+					fmt.Print(comma)
+					printField(e, in+1)
+					comma = ", "
+				}
+				newLine = ",\n"
+			}
+			fmt.Printf(",\n%s}", indent(in))
+
+		case v.Index(0).Kind() == reflect.Array: // if the first elem is an array
+			fmt.Printf("{")
+			comma := ""
+			fmt.Print(indent(in))
+			for i := 0; i < v.Len(); i++ {
+				fmt.Printf("%s\n%s", comma, indent(in+1))
+				printField(v.Index(i), in+1)
+				comma = ","
+			}
+			fmt.Printf(",\n%s}", indent(in))
+
+		default: // one line array
+
+			fmt.Printf("{ ")
+			comma := ""
+			for i := 0; i < v.Len(); i++ {
+
+				fmt.Printf("%s", comma)
+				printField(v.Index(i), in+1)
+				comma = ", "
+			}
+			fmt.Printf("}")
+		}
+
+	case reflect.Float64:
+		f := v.Float()
+		r := int(math.Round(f))
+
+		fmt.Printf("%4d", r)
+
+	default:
+		panic("unexpected kind " + reflect.TypeOf(v).Kind().String())
+	}
+}
+
+type Index struct {
+	s []int
+}
+
+func (t *Coeffs) Loop() iter.Seq[Index] {
+	return func(yield func(Index) bool) {
+		typ := reflect.TypeOf(*t.ToEvalType())
+		v := reflect.ValueOf(*t.ToEvalType())
+
+		for ix := 0; ix < typ.NumField(); ix++ {
+			if !slices.Contains(Targets[:], typ.Field(ix).Name) {
+				continue
+			}
+
+			e := v.Field(ix)
+			if e.Kind() != reflect.Array {
+				panic("expected array " + e.Kind().String())
+			}
+
+			if !recurse(yield, Index{[]int{ix}}, e) {
+				return
+			}
+		}
+	}
+}
+
+func recurse(yield func(Index) bool, ix Index, v reflect.Value) bool {
+
+	switch v.Kind() {
+
+	case reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			if !recurse(yield, Index{append(ix.s, i)}, v.Index(i)) {
+				return false
+			}
+		}
+
+	case reflect.Float64:
+		return yield(ix)
+
+	}
+
+	return true
+}
+func (t *Coeffs) At(ix Index) *float64 {
+	v := reflect.ValueOf(t)
+	ixs := ix.s
+
+	return recurseAt(ixs[1:], v.Elem().Field(ixs[0]))
+}
+
+func recurseAt(ixs []int, v reflect.Value) *float64 {
+	switch v.Kind() {
+
+	case reflect.Array:
+		return recurseAt(ixs[1:], v.Index(ixs[0]))
+
+	case reflect.Float64:
+		return v.Addr().Interface().(*float64)
+	}
+	panic("unreachable")
+}
+
