7cf96c45eb564f594f64d4ff1c60a51245f5da61
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-03-30 17:04:50 +0100

    added assertion for PV corruption

--- search/search.go
+++ search/search.go
@@ -95,7 +95,7 @@ func Search(b *board.Board, d Depth, sst *State) (score Score, move move.SimpleM
 		var scoreSample Score
 
 		for !awOk {
-			scoreSample = AlphaBeta(b, alpha, beta, d, 0, true, false, sst)
+			scoreSample = AlphaBeta(b, alpha, beta, d, 0, true, false, sst, byte(b.STM))
 
 			switch {
 
@@ -187,7 +187,7 @@ func scInfo(score Score) string {
 
 // AlphaBeta performs an alpha beta search to depth d, and then transitions
 // into Quiesence() search.
-func AlphaBeta(b *board.Board, alpha, beta Score, d, ply Depth, pvN, cutN bool, sst *State) Score {
+func AlphaBeta(b *board.Board, alpha, beta Score, d, ply Depth, pvN, cutN bool, sst *State, check byte) Score {
 
 	transpT := sst.tt
 	sst.pv.setNull(ply)
@@ -203,6 +203,9 @@ func AlphaBeta(b *board.Board, alpha, beta Score, d, ply Depth, pvN, cutN bool, 
 
 		case transp.PVNode:
 			if transpE.From|transpE.To != 0 {
+				if (byte(ply & 1) ^ byte(b.STM)) != check {
+					panic("oops")
+				}
 				sst.pv.setTip(ply, transpE.SimpleMove)
 			}
 			return transpE.Value
@@ -248,7 +251,7 @@ func AlphaBeta(b *board.Board, alpha, beta Score, d, ply Depth, pvN, cutN bool, 
 
 			rd := max(0, d-3)
 
-			value := -AlphaBeta(b, -beta, -beta+1, rd, ply, false, !cutN, sst)
+			value := -AlphaBeta(b, -beta, -beta+1, rd, ply, false, !cutN, sst, check ^1)
 
 			b.UndoNullMove(enP)
 
@@ -316,7 +319,7 @@ func AlphaBeta(b *board.Board, alpha, beta Score, d, ply Depth, pvN, cutN bool, 
 		// move, which is likely to be the hash move.
 		if d > 1 && quietCnt > 2 && !inCheck {
 			rd := lmr(d, moveCnt-1, improving, pvN, cutN)
-			value = -AlphaBeta(b, -alpha-1, -alpha, rd, ply+1, false, !cutN, sst)
+			value = -AlphaBeta(b, -alpha-1, -alpha, rd, ply+1, false, !cutN, sst, check)
 
 			if value <= alpha {
 				if value > maxim {
@@ -331,7 +334,7 @@ func AlphaBeta(b *board.Board, alpha, beta Score, d, ply Depth, pvN, cutN bool, 
 		}
 
 		// null window search failed (meaning didn't fail low).
-		value = -AlphaBeta(b, -beta, -alpha, d-1, ply+1, true, false, sst)
+		value = -AlphaBeta(b, -beta, -alpha, d-1, ply+1, true, false, sst, check)
 
 		b.UndoMove(m)
 		sst.hstack.pop()
@@ -381,6 +384,9 @@ func AlphaBeta(b *board.Board, alpha, beta Score, d, ply Depth, pvN, cutN bool, 
 			// value > alpha
 			failLow = false
 			alpha = value
+			if (byte(ply&1) ^ byte(b.STM)) != check {
+				panic("oops2")
+			}
 			sst.pv.insert(ply, m.SimpleMove)
 		}
 
