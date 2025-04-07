0e5f9e5bcd4bda778d32b8a58974d473ce00b0e2
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-04-07 13:06:47 +0100

    fix tt clear

--- transp/transp.go
+++ transp/transp.go
@@ -70,7 +70,7 @@ func (t Table) HashFull() int {
 func (t *Table) Clear() {
 	t.cnt = 0
 	for ix := range t.data {
-		t.data[ix].Depth = 0
+		t.data[ix].Hash = 0
 	}
 }
 
@@ -87,7 +87,7 @@ func (t *Table) Insert(hash board.Hash, d, tfCnt Depth, sm move.SimpleMove, valu
 		return
 	}
 
-	if t.data[ix].Depth == 0 {
+	if t.data[ix].Hash == 0 {
 		t.cnt++
 	}
 
