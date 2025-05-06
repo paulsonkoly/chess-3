62e2d277d504ff19606fc528fe47610fb6483363
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-05-06 08:49:53 +0100

    isolated pawns (#107)
    bench 2131053

--- tools/tuner/tuning/tuning.go
+++ tools/tuner/tuning/tuning.go
@@ -18,7 +18,7 @@ var Targets = [...]string{
 	"TempoBonus",
 	"MobilityKnight", "MobilityBishop", "MobilityRook",
 	"KingAttackPieces", "SafeChecks", "KingShelter",
-	"ProtectedPasser", "PasserKingDist", "PasserRank", "DoubledPawns",
+	"ProtectedPasser", "PasserKingDist", "PasserRank", "DoubledPawns", "IsolatedPawns",
 	"KnightOutpost", "ConnectedRooks", "BishopPair",
 }
 
