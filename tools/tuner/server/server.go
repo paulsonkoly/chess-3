7405c641bd3632fd0ac248edbcda4ee57e195c1d
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-10-15 14:23:06 +0100

    protoc

--- tools/tuner/server/server.go
+++ tools/tuner/server/server.go
@@ -1,4 +1,215 @@
 package server
 
+import (
+	"fmt"
+	"iter"
+	"net"
+	"time"
+
+	pb "github.com/paulsonkoly/chess-3/tools/tuner/grpc/tuner"
+	"google.golang.org/grpc"
+)
+
+const (
+	// NumLinesInBatch determines how the epd file is split into batches. A batch
+	// completion implies the coefficients update.
+	NumLinesInBatch = 100_000
+	// NumChunksInBatch determines how a batch is split into chunks. A chunk is a
+	// unique work iterm handed over to clients.
+	NumChunksInBatch = 16
+	// JobTTL is job time to live duration in seconds.
+	JobTTL = 600
+	// JobQueueDepth determines how many jobs the EPD processor can produce
+	// before its stalled back. Should not be smaller than NumChunksInBatch
+	// otherwise we can create jobs for a whole batch simultaniously. It can be
+	// bigger slightly, allowing jobs with passed TTL to live together with newly
+	// dispatched jobs, in case a passed TTL job eventually finishes.
+	JobQueueDepth    = 20
+	ResultQueueDepth = 10
+	// ClientWaitTime is the duration in ms the server waits for results before
+	// creating new jobs.
+	ClientWaitTime = 100
+)
+
+type Coeffs []float64
+
+type Chunk struct {
+	start int
+	end   int
+}
+
+type ServerChunk struct {
+	Chunk
+	completed bool
+	jobs      []ServerJob
+}
+
+type batchChunks []ServerChunk
+
+func (bc batchChunks) ConsistencyCheck() {
+	// consistency check
+	for _, c := range bc {
+		if c.completed && len(c.jobs) == 0 {
+			panic("chunk is marked completed, with no associated jobs")
+		}
+	}
+}
+
+func (bc batchChunks) Incomplete() iter.Seq2[int, ServerChunk] {
+	return func(yield func(int, ServerChunk) bool) {
+
+		// first look for a chunk that has no jobs
+		for i, chunk := range bc {
+			if len(chunk.jobs) == 0 {
+				if !yield(i, chunk) {
+					return
+				}
+			}
+		}
+
+		// all chunks must have jobs at this point
+		allCompleted := false
+		for !allCompleted {
+
+			allCompleted = true
+
+			// look for the chunk that has earliest deadline, if a chunk has multiple
+			// jobs its deadline is the latest of its jobs deadlines. Ignore
+			// completed chunks.
+			minTime := time.Now().Add(time.Duration(10) * time.Hour)
+			minIx := -1
+
+			for i, chunk := range bc {
+				if !chunk.completed {
+					maxTime := time.Now().Add(time.Duration(-10) * time.Hour)
+					for _, j := range chunk.jobs {
+						if j.deadline.After(maxTime) {
+							maxTime = j.deadline
+						}
+					}
+
+					// TODO we can add extra conditions here that prevent scheduling jobs
+					// for recently scheduled chunks.
+					if maxTime.Before(minTime) {
+						minTime = maxTime
+						minIx = i
+					}
+
+					allCompleted = false
+				}
+			}
+
+			if minIx != -1 {
+				if !yield(minIx, bc[minIx]) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func (bc batchChunks) Match(r Result) (ix int, ok bool) {
+	for i, chunk := range bc {
+		for _, job := range chunk.jobs {
+			if job.UUID == r.UUID {
+				return i, true
+			}
+		}
+	}
+	return 0, false
+}
+
+type Job struct {
+	UUID string
+	Chunk
+}
+
+type ServerJob struct {
+	deadline time.Time
+	Job
+}
+
+type Result struct {
+	UUID      string
+	Gradients []float64
+}
+
 func Run() {
+	jobQueue := make(chan Job, JobQueueDepth)
+	resultQueue := make(chan Result, ResultQueueDepth)
+
+	s := tunerServer{jobQueue: jobQueue, resultQueue: resultQueue}
+
+	go epdProcess(jobQueue, resultQueue)
+
+	// listen via GRPC
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 9999))
+	if err != nil {
+		panic("failed to listen")
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterTunerServer(grpcServer, s)
+	grpcServer.Serve(lis)
+}
+
+func epdProcess(jobQueue chan<- Job, resultQueue <-chan Result) {
+	epdFile := epd.Load("filename.epd")
+
+	coeffs := initialCoeffs
+
+	for epoch := 1; true; epoch++ {
+		for batch := range epdFile.Batches(NumLinesInBatch) {
+
+			grads := Coeffs{}
+
+			// gather the chunks in the batch and create server tracking structures
+			chunks := make(batchChunks, 0, NumChunksInBatch)
+			for chunk := range batch.Chunks(NumChunksInBatch) {
+				chunks = append(chunks, ServerChunk{Chunk: chunk})
+			}
+
+			// while there is an incomplete chunk in the batch
+			for i, chunk := range chunks.Incomplete() {
+
+				//create a job for the batch
+				job := ServerJob{
+					deadline: time.Now().Add(600 * time.Second),
+					Job:      Job{Chunk: chunk.Chunk},
+				}
+
+				// put the job in the tracking structures
+				chunks[i].jobs = append(chunks[i].jobs, job)
+
+				// send the job to the client handler
+				jobQueue <- job.Job
+
+				// register results
+				select {
+				case result := <-resultQueue:
+					// validate result coming from client and search for a matching job in our structures
+					if ix, ok := chunks.Match(result); ok {
+						// if already completed ignore
+						if !chunks[ix].completed {
+							grads.Add(result.Gradients)
+							chunks[ix].completed = true
+						}
+					}
+
+				case <-time.After(ClientWaitTime * time.Millisecond):
+					// no results coming in yet
+				}
+			}
+
+			// batch completed, add the gradient vector to the coeffs
+			coeffs.Add(grads)
+		}
+
+		// epoch completed, output coeffs
+		fmt.Println(coeffs)
+
+		// shuffle
+		epdFile.Shuffle()
+	}
 }
---
+++ tools/tuner/server/server_grpc_shim.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"context"
+
+	pb "github.com/paulsonkoly/chess-3/tools/tuner/grpc/tuner"
+)
+
+type tunerServer struct {
+	pb.UnimplementedTunerServer
+	jobQueue    chan Job
+	resultQueue chan Result
+}
+
+func (s tunerServer) RequestJob(_ context.Context, _ *pb.JobRequest) (*pb.JobResponse, error) {
+	job := <-s.jobQueue
+
+	result := pb.JobResponse{
+		JobUuid: job.UUID,
+	}
+
+	return &result, nil
+}
+
+func (s tunerServer) RegisterResult(_ context.Context, r *pb.ResultRequest) (*pb.ResultAck, error) {
+	result := Result{UUID: r.JobUuid}
+	s.resultQueue <- result
+
+	return nil, nil
+}
