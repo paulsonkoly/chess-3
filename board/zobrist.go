e5cfa32194a83d8d867387c115782de4c43163b9
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-11-15 14:21:43 +0000

    board hash refactor
    This is motivated by the optimised ParseFEN vs the original FromFEN
    functions deviating in behaviour. For tuner tools the optimised
    allocation free ParseFEN is useful, but we need to be able to set the
    Hash on the board in case we need to execute moves on the board.
    
    exempt from SPRT
    
    bench 10918860

---
+++ board/zobrist.go
@@ -0,0 +1,76 @@
+package board
+
+import (
+	"math/rand/v2"
+
+	. "github.com/paulsonkoly/chess-3/types"
+)
+
+// Hash is a chess position Zobrist hash.
+type Hash uint64
+
+// zobrist hashes
+var (
+	piecesRand   [2][7][64]Hash
+	stmRand      Hash
+	castlingRand [4]Hash
+	epFileRand   [8]Hash
+)
+
+var r rand.Source = rand.NewPCG(0xdeadbeeff0dbaad, 0xbaadf00ddeadbeef)
+
+func init() {
+	for i := range piecesRand {
+		for j := range piecesRand[i] {
+			for k := range piecesRand[i][j] {
+				if j == int(NoPiece) {
+					piecesRand[i][j][k] = 0
+				} else {
+					piecesRand[i][j][k] = Hash(r.Uint64())
+				}
+			}
+		}
+	}
+	stmRand = Hash(r.Uint64())
+	for i := range castlingRand {
+		castlingRand[i] = Hash(r.Uint64())
+	}
+	for i := range epFileRand {
+		epFileRand[i] = Hash(r.Uint64())
+	}
+}
+
+// CalculateHash calculates the Zobrist hash for b from scratch. Normally it
+// should not be used, b.Hash would give you a cached value of the same if b is
+// obtained by making moves on b.
+func (b Board) calculateHash() Hash {
+	var hash Hash
+
+	for color := White; color <= Black; color++ {
+		occ := b.Colors[color]
+
+		for piece := BitBoard(0); occ != 0; occ = occ ^ piece {
+			piece = occ & -occ
+
+			sq := piece.LowestSet()
+
+			hash ^= piecesRand[color][b.SquaresToPiece[sq]][sq]
+		}
+	}
+
+	if b.STM == Black {
+		hash ^= stmRand
+	}
+
+	for i, r := range castlingRand {
+		if b.CRights&(1<<i) != 0 {
+			hash ^= r
+		}
+	}
+
+	if b.EnPassant != 0 {
+		hash ^= epFileRand[b.EnPassant%8]
+	}
+
+	return hash
+}
