009477616e21468620e3eb0861982e51b23d9b61
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-12-03 15:21:02 +0000

    progress

--- tools/datagen/datagen.go
+++ tools/datagen/datagen.go
@@ -3,15 +3,20 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"fmt"
 	"math/rand/v2"
+	"os"
 	"runtime"
 	"sync"
+
 	_ "modernc.org/sqlite"
 
 	"github.com/paulsonkoly/chess-3/board"
 	"github.com/paulsonkoly/chess-3/eval"
 	"github.com/paulsonkoly/chess-3/move"
 	"github.com/paulsonkoly/chess-3/movegen"
+	"github.com/paulsonkoly/chess-3/search"
+	"github.com/paulsonkoly/chess-3/transp"
 	"github.com/paulsonkoly/chess-3/types"
 )
 
@@ -21,7 +26,10 @@ type Config struct {
 	gameCount     int
 	openingDepth  int
 	openingMargin int
+	softNodes     int
+	hardNodes     int
 	threads       int
+	dbFile        string
 }
 
 var config = Config{}
@@ -32,6 +40,9 @@ func main() {
 	flag.IntVar(&config.openingDepth, "openingDepth", 8, "number of random generated opening moves")
 	flag.IntVar(&config.openingMargin, "openingMargin", 300, "margin for what's considered to be balanced opening (cp)")
 	flag.IntVar(&config.threads, "threads", runtime.NumCPU()-1, "number of threads")
+	flag.IntVar(&config.softNodes, "softNodes", 15_000, "soft node count for search")
+	flag.IntVar(&config.hardNodes, "hardNodes", 8_000_000, "hard node count for search")
+	flag.StringVar(&config.dbFile, "dbFile", "datagen.db", "file name for the database")
 	flag.Parse()
 
 	gamesPerThread := (config.gameCount + config.threads - 1) / config.threads
@@ -45,7 +56,7 @@ func main() {
 			gamesPerThread = config.gameCount - t*gamesPerThread
 		}
 
-		workersWG.Go(func() { generateGames(gamesPerThread, games) })
+		workersWG.Go(func() { NewGenerator().Games(gamesPerThread, games) })
 	}
 
 	writerWG := sync.WaitGroup{}
@@ -59,8 +70,8 @@ func main() {
 }
 
 type Game struct {
-	wdl byte
-	pos []Position
+	wdl       byte
+	positions []Position
 }
 
 type Position struct {
@@ -69,42 +80,129 @@ type Position struct {
 	eval types.Score
 }
 
-func generateGames(count int, out chan<- *Game) {
-	b := board.StartPos()
+type Generator struct {
+	search    *search.Search
+	moveStore *move.Store
+}
+
+func NewGenerator() Generator {
+	s := search.New(1 * transp.MegaBytes)
 	ms := move.NewStore()
 
-	defer ms.Clear()
+	return Generator{search: s, moveStore: ms}
+}
 
-	for range config.openingDepth {
-		ms.Push()
-		movegen.GenMoves(ms, b)
-
-		moves := ms.Frame()
-
-		if len(moves) < 1 {
-			// we got into a position where there are no moves in the opening, highly unlikely
-			return
-		}
-
-		chose := rand.IntN(len(moves))
-		move := moves[chose]
-		ms.Pop()
-
-		b.MakeMove(&move)
-	}
-
-	// verify the opening in that it's resulted in a roughly equal position
-	staticEval := eval.Eval(b, &eval.Coefficients)
-	if staticEval < -types.Score(config.openingMargin) || staticEval > types.Score(config.openingMargin) {
-		return
+func (g Generator) Games(count int, out chan<- *Game) {
+	s := search.New(1 * transp.MegaBytes)
+	for range count {
+		s.Clear()
+		g.Game(out)
 	}
 }
 
-func writer(in <-chan *Game) {
-	db, err := sql.Open("sqlite", "file.sql")
+func (g Generator) Game(out chan<- *Game) {
+	g.search.Clear()
+
+	b := g.Opening()
+
+	finished := false
+	for !finished {
+		g.search.Go(b,
+			search.WithSoftNodes(config.softNodes),
+			search.WithNodes(config.hardNodes))
+	}
+}
+
+func (g *Generator) Opening() *board.Board {
+	success := false
+	var b *board.Board
+
+	for !success {
+		g.moveStore.Clear()
+		b = board.StartPos()
+
+		success = true
+		for range config.openingDepth {
+			g.moveStore.Push()
+			movegen.GenMoves(g.moveStore, b)
+			moves := g.moveStore.Frame()
+
+			if len(moves) < 1 {
+				success = false
+				break
+			}
+
+			move := &moves[rand.IntN(len(moves))]
+			b.MakeMove(move)
+			if movegen.InCheck(b, b.STM.Flip()) { // pseudo legality check
+				success = false
+				break
+			}
+		}
+
+		if success {
+			score := eval.Eval(b, &eval.Coefficients)
+			if score < types.Score(config.openingMargin) || score > types.Score(config.openingMargin) {
+				success = false
+			}
+		}
+	}
+
+	return b
+}
+
+func writer(games <-chan *Game) {
+	db, err := sql.Open("sqlite", config.dbFile)
 	if err != nil {
 		panic(err)
 	}
-	for game := range in {
+	defer db.Close()
+
+	// back to back error count
+	b2bErrorCnt := 0
+
+	for game := range games {
+
+		tx, err := db.Begin()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to create tx %v", err)
+			b2bErrorCnt++
+			continue
+		}
+
+		if _, err := db.Exec("insert into games values ()", game.wdl); err != nil {
+			fmt.Fprintf(os.Stderr, "insert into games failed %v", err)
+			b2bErrorCnt++
+			goto Fin
+		}
+
+		for _, pos := range game.positions {
+			if _, err := db.Exec("insert into positions values ()", pos.fen); err != nil {
+				b2bErrorCnt++
+				goto Fin
+			}
+		}
+
+		b2bErrorCnt = 0
+
+	Fin:
+
+		switch {
+
+		case b2bErrorCnt == 0:
+			if err := tx.Commit(); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to commit tx %v", err)
+				b2bErrorCnt++
+			}
+
+		case b2bErrorCnt > 3:
+			panic("more than 3 errors happened back to back in the writer, giving up")
+
+		default:
+			if err := tx.Rollback(); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to rollback tx %v", err)
+				b2bErrorCnt++
+			}
+		}
 	}
 }
