d0f77b7f79d6d8e0ea4f734137a40b529b4e359c
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-12-18 17:44:19 +0000

    some godoc chores (#187)
    exempt from SPRT
    
    bench 10987804

--- heur/heur.go
+++ heur/heur.go
@@ -1,3 +1,12 @@
+// Package heur provides move ordering heuristics.
+//
+// # Move ordering stages
+//
+//   - hash: [HashMove]
+//   - good captures: [Captures] ... [HashMove]
+//   - quiets: -6*[MaxHistory]..6*[MaxHistory]
+//     (3 * cont[0] + 2 * cont[1] + hist) each <= [MaxHistory]
+//   - bad captures: -Inf..-[Captures]
 package heur
 
 import (
@@ -8,20 +17,14 @@ import (
 // purposes.
 var PieceValues = [...]Score{0, 100, 300, 300, 500, 900, Inf}
 
-// Move ordering stages
-//
-// | hash | .. Good captures .. | .. Continuation .. | .. History .. | .. Quiet .. | .. unused .. | .. Bad captures .. |
-//
-//	 ^                         ^                                    ^       ^                      ^
-//	HashMove            Captures                                Quiet       0                      -Captures
 const (
 	// HashMove is assigned to a move from Hash, either PV or fail-high.
 	HashMove = Score(15000)
 	// Captures is the minimal score for captures, actual score is this plus SEE.
 	Captures = Score(8192)
+	// MaxHistory is the maximal absolute value in either the history or the continuation stores.
+	MaxHistory = Score(1024)
 )
-
-const MaxHistory = 1024
 
 func init() {
 	// 1 * history + 2 * continuation[1] + 3 * continuation[0]
