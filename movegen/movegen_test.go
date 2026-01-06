5ddcbc76b1bbfe7d4a183fc64b35b4d12c662dac
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-01-06 17:20:20 +0000

    test refactor chores (#218)
    exempt from sprt
    
    bench 14298810

--- movegen/movegen_test.go
+++ movegen/movegen_test.go
@@ -15,49 +15,49 @@ func TestMoves(t *testing.T) {
 	tests := []struct {
 		name string // description of this test case
 		// Named input parameters for target function.
-		b    *board.Board
+		fen  string
 		want []string
 	}{
 		{
 			name: "simple king move",
-			b:    Must(board.FromFEN("8/8/8/8/8/4K3/8/k7 w - - 0 1")),
+			fen:  "8/8/8/8/8/4K3/8/k7 w - - 0 1",
 			want: []string{"e3d2", "e3e2", "e3f2", "e3d3", "e3f3", "e3d4", "e3e4", "e3f4"},
 		},
 		{
 			name: "king in the corner",
-			b:    Must(board.FromFEN("8/8/8/8/8/8/K7/7k b - - 0 1")),
+			fen:  "8/8/8/8/8/8/K7/7k b - - 0 1",
 			want: []string{"h1h2", "h1g2", "h1g1"},
 		},
 		{
 			name: "simple knight move",
-			b:    Must(board.FromFEN("8/8/8/8/8/4N3/8/k6K w - - 0 1")),
+			fen:  "8/8/8/8/8/4N3/8/k6K w - - 0 1",
 			want: []string{"e3c4", "e3d5", "e3f5", "e3g4", "e3c2", "e3d1", "e3f1", "e3g2", "h1g1", "h1g2", "h1h2"},
 		},
 		{
 			name: "knight in the corner",
-			b:    Must(board.FromFEN("k7/8/8/8/8/8/8/K6N w - - 0 1")),
+			fen:  "k7/8/8/8/8/8/8/K6N w - - 0 1",
 			want: []string{"h1f2", "h1g3", "a1a2", "a1b2", "a1b1"},
 		},
 		{
 			name: "simple bishop move",
-			b:    Must(board.FromFEN("k7/8/8/8/8/3B4/8/7K w - - 0 1")),
+			fen:  "k7/8/8/8/8/3B4/8/7K w - - 0 1",
 			want: []string{
 				"d3c2", "d3b1", "d3e2", "d3f1", "d3c4", "d3b5", "d3a6", "d3e4", "d3f5", "d3g6", "d3h7", "h1g1", "h1g2", "h1h2",
 			},
 		},
 		{
 			name: "bishop in the corner",
-			b:    Must(board.FromFEN("k7/8/8/8/8/8/8/B6K w - - 0 1")),
+			fen:  "k7/8/8/8/8/8/8/B6K w - - 0 1",
 			want: []string{"a1b2", "a1c3", "a1d4", "a1e5", "a1f6", "a1g7", "a1h8", "h1g1", "h1g2", "h1h2"},
 		},
 		{
 			name: "bishop blocked by friendly",
-			b:    Must(board.FromFEN("k7/8/8/8/8/2K5/1B6/8 w - - 0 1")),
+			fen:  "k7/8/8/8/8/2K5/1B6/8 w - - 0 1",
 			want: []string{"b2a3", "b2a1", "b2c1", "c3b3", "c3b4", "c3c2", "c3c4", "c3d2", "c3d3", "c3d4"},
 		},
 		{
 			name: "simple rook move",
-			b:    Must(board.FromFEN("k7/8/8/8/4R3/8/8/7K w - - 0 1")),
+			fen:  "k7/8/8/8/4R3/8/8/7K w - - 0 1",
 			want: []string{
 				"e4d4", "e4c4", "e4b4", "e4a4", "e4h4", "e4g4", "e4f4", "e4e5", "e4e6", "e4e7", "e4e8", "e4e3", "e4e2", "e4e1",
 				"h1g1", "h1g2", "h1h2",
@@ -65,7 +65,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "rook in the corner",
-			b:    Must(board.FromFEN("k7/8/8/8/8/8/8/R6K w - - 0 1")),
+			fen:  "k7/8/8/8/8/8/8/R6K w - - 0 1",
 			want: []string{
 				"a1a2", "a1a3", "a1a4", "a1a5", "a1a6", "a1a7", "a1a8", "a1b1", "a1c1", "a1d1", "a1e1", "a1f1", "a1g1", "h1g1",
 				"h1g2", "h1h2",
@@ -73,7 +73,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "rook blocked by friendly",
-			b:    Must(board.FromFEN("k7/8/8/8/8/2K5/2R5/8 w - - 0 1")),
+			fen:  "k7/8/8/8/8/2K5/2R5/8 w - - 0 1",
 			want: []string{
 				"c2b2", "c2a2", "c2d2", "c2e2", "c2f2", "c2g2", "c2h2", "c2c1", "c3b3", "c3b4", "c3b2", "c3c4", "c3d2", "c3d3",
 				"c3d4",
@@ -81,7 +81,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "simple queen move",
-			b:    Must(board.FromFEN("k7/8/8/8/4Q3/8/8/7K w - - 0 1")),
+			fen:  "k7/8/8/8/4Q3/8/8/7K w - - 0 1",
 			want: []string{
 				"e4d4", "e4c4", "e4b4", "e4a4", "e4h4", "e4g4", "e4f4", "e4e5", "e4e6", "e4e7", "e4e8", "e4e3", "e4e2", "e4e1",
 				"e4f5", "e4g6", "e4h7", "e4f3", "e4g2", "e4d5", "e4c6", "e4b7", "e4a8", "e4d3", "e4c2", "e4b1", "h1g1", "h1g2",
@@ -90,7 +90,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "queen in the corner",
-			b:    Must(board.FromFEN("k7/8/8/8/8/8/8/Q6K w - - 0 1")),
+			fen:  "k7/8/8/8/8/8/8/Q6K w - - 0 1",
 			want: []string{
 				"a1a2", "a1a3", "a1a4", "a1a5",
 				"a1a6", "a1a7", "a1a8",
@@ -103,7 +103,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "queen blocked by friendly",
-			b:    Must(board.FromFEN("8/8/8/2k5/8/2K5/2Q5/8 w - - 0 1")),
+			fen:  "8/8/8/2k5/8/2K5/2Q5/8 w - - 0 1",
 			want: []string{
 				"c2b1", "c2c1", "c2d1", "c2a2", "c2b2", "c2d2", "c2e2", "c2f2", "c2g2", "c2h2", "c2b3", "c2d3", "c2a4", "c2e4",
 				"c2f5", "c2g6", "c2h7", "c3b2", "c3d2", "c3b3", "c3d3",
@@ -111,82 +111,82 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "single pawn push forward as white",
-			b:    Must(board.FromFEN("4k3/8/8/8/4P3/8/8/K7 w - - 0 1")),
+			fen:  "4k3/8/8/8/4P3/8/8/K7 w - - 0 1",
 			want: []string{"e4e5", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "single pawn push forward as black",
-			b:    Must(board.FromFEN("7K/8/8/8/4p3/8/8/k7 b - - 0 1")),
+			fen:  "7K/8/8/8/4p3/8/8/k7 b - - 0 1",
 			want: []string{"e4e3", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "blocked pawn push forward as white",
-			b:    Must(board.FromFEN("8/8/8/4k3/4P3/8/8/K7 w - - 0 1")),
+			fen:  "8/8/8/4k3/4P3/8/8/K7 w - - 0 1",
 			want: []string{"a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "blocked pawn push forward as black",
-			b:    Must(board.FromFEN("8/8/8/8/4p3/4K3/8/k7 b - - 0 1")),
+			fen:  "8/8/8/8/4p3/4K3/8/k7 b - - 0 1",
 			want: []string{"a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "double pawn push forward as white",
-			b:    Must(board.FromFEN("7k/8/8/8/8/8/4P3/K7 w - - 0 1")),
+			fen:  "7k/8/8/8/8/8/4P3/K7 w - - 0 1",
 			want: []string{"e2e3", "e2e4", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "double pawn push forward as black",
-			b:    Must(board.FromFEN("7K/4p3/8/8/8/8/8/k7 b - - 0 1")),
+			fen:  "7K/4p3/8/8/8/8/8/k7 b - - 0 1",
 			want: []string{"e7e6", "e7e5", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "double pawn push blocked by a piece directly in front",
-			b:    Must(board.FromFEN("8/8/8/8/8/4k3/4P3/K7 w - - 0 1")),
+			fen:  "8/8/8/8/8/4k3/4P3/K7 w - - 0 1",
 			want: []string{"a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "double pawn push blocked by a piece 2 squares in front",
-			b:    Must(board.FromFEN("8/8/8/8/4k3/8/4P3/K7 w - - 0 1")),
+			fen:  "8/8/8/8/4k3/8/4P3/K7 w - - 0 1",
 			want: []string{"e2e3", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "pawn capture",
-			b:    Must(board.FromFEN("7k/8/8/8/3n4/4P3/8/K7 w - - 0 1")),
+			fen:  "7k/8/8/8/3n4/4P3/8/K7 w - - 0 1",
 			want: []string{"e3e4", "e3d4", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "pawn capture on AFile testing for wrap to HFile",
-			b:    Must(board.FromFEN("7k/8/8/8/1n5n/P7/8/K7 w - - 0 1")),
+			fen:  "7k/8/8/8/1n5n/P7/8/K7 w - - 0 1",
 			want: []string{"a3a4", "a3b4", "a1b1", "a1b2"},
 		},
 		{
 			name: "pawn promotion (push)",
-			b:    Must(board.FromFEN("7k/4P3/8/8/8/8/8/K7 w - - 0 1")),
+			fen:  "7k/4P3/8/8/8/8/8/K7 w - - 0 1",
 			want: []string{"e7e8q", "e7e8r", "e7e8b", "e7e8n", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "pawn promotion blocked (push)",
-			b:    Must(board.FromFEN("8/8/8/8/8/8/6p1/k5K1 b - - 0 1")),
+			fen:  "8/8/8/8/8/8/6p1/k5K1 b - - 0 1",
 			want: []string{"a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "pawn promotion (capture)",
-			b:    Must(board.FromFEN("3nn2k/4P3/8/8/8/8/8/K7 w - - 0 1")),
+			fen:  "3nn2k/4P3/8/8/8/8/8/K7 w - - 0 1",
 			want: []string{"e7d8q", "e7d8r", "e7d8b", "e7d8n", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "pawn promotion (capture or push)",
-			b:    Must(board.FromFEN("3n3k/4P3/8/8/8/8/8/K7 w - - 0 1")),
+			fen:  "3n3k/4P3/8/8/8/8/8/K7 w - - 0 1",
 			want: []string{"e7d8q", "e7d8r", "e7d8b", "e7d8n", "e7e8q", "e7e8r", "e7e8b", "e7e8n", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "en passant",
-			b:    Must(board.FromFEN("7k/8/8/2Pp4/8/8/8/K7 w - d6 0 1")),
+			fen:  "7k/8/8/2Pp4/8/8/8/K7 w - d6 0 1",
 			want: []string{"c5c6", "c5d6", "a1b1", "a1b2", "a1a2"},
 		},
 		{
 			name: "regression #1",
-			b:    Must(board.FromFEN("rnbqkbnr/1ppppppp/8/p7/8/7P/PPPPPPP1/RNBQKBNR w - - 0 1")),
+			fen:  "rnbqkbnr/1ppppppp/8/p7/8/7P/PPPPPPP1/RNBQKBNR w - - 0 1",
 			want: []string{
 				"a2a3", "b2b3", "c2c3", "d2d3", "e2e3", "f2f3", "g2g3", "h3h4", "a2a4", "b2b4", "c2c4", "d2d4", "e2e4", "f2f4",
 				"g2g4", "b1a3", "b1c3", "g1f3", "h1h2",
@@ -194,7 +194,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "regression #2",
-			b:    Must(board.FromFEN("rnbqkbnr/1ppppppp/8/p7/1P6/8/P1PPPPPP/RNBQKBNR w - - 0 1")),
+			fen:  "rnbqkbnr/1ppppppp/8/p7/1P6/8/P1PPPPPP/RNBQKBNR w - - 0 1",
 			want: []string{
 				"a2a3", "c2c3", "d2d3", "e2e3", "f2f3", "g2g3", "h2h3", "b4b5", "a2a4", "c2c4", "d2d4", "e2e4", "f2f4", "g2g4",
 				"h2h4", "b4a5", "b1a3", "b1c3", "g1f3", "g1h3", "c1b2", "c1a3",
@@ -202,7 +202,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "regression #3",
-			b:    Must(board.FromFEN("rnbqkbnr/1ppppppp/8/p7/8/N6N/PPPPPPPP/R1BQKB1R b - - 0 1")),
+			fen:  "rnbqkbnr/1ppppppp/8/p7/8/N6N/PPPPPPPP/R1BQKB1R b - - 0 1",
 			want: []string{
 				"a5a4", "b7b6", "c7c6", "d7d6", "e7e6", "f7f6", "g7g6", "h7h6", "b7b5", "c7c5", "d7d5", "e7e5", "f7f5", "g7g5",
 				"h7h5", "b8a6", "b8c6", "g8f6", "g8h6", "a8a6", "a8a7",
@@ -210,7 +210,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "regression #4",
-			b:    Must(board.FromFEN("rnbqkbnr/1ppppppp/p7/1N6/8/8/PPPPPPPP/R1BQKBNR b - - 0 1")),
+			fen:  "rnbqkbnr/1ppppppp/p7/1N6/8/8/PPPPPPPP/R1BQKBNR b - - 0 1",
 			want: []string{
 				"a6a5", "b7b6", "c7c6", "d7d6", "e7e6", "f7f6", "g7g6", "h7h6", "c7c5", "d7d5", "e7e5", "f7f5", "g7g5", "h7h5",
 				"a6b5", "b8c6", "g8f6", "g8h6", "a8a7",
@@ -218,7 +218,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "regression #5",
-			b:    Must(board.FromFEN("rnbq3r/pp1Pbpkp/2p3p1/6P1/2B5/8/PPP1Nn1P/RNBQ1K1R b - - 0 1")),
+			fen:  "rnbq3r/pp1Pbpkp/2p3p1/6P1/2B5/8/PPP1Nn1P/RNBQ1K1R b - - 0 1",
 			want: []string{
 				"c6c5", "a7a6", "b7b6", "f7f6", "h7h6", "a7a5", "b7b5", "f7f5", "h7h5", "f2d1", "f2h1", "f2d3", "f2h3", "f2e4",
 				"f2g4", "b8a6", "b8d7", "e7a3", "e7b4", "e7c5", "e7g5", "e7d6", "e7f6", "e7f8", "c8d7", "h8e8", "h8f8", "h8g8",
@@ -227,7 +227,7 @@ func TestMoves(t *testing.T) {
 		},
 		{
 			name: "regression #6",
-			b:    Must(board.FromFEN("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPPBN1PP/R3KN1n w Q - 0 1")),
+			fen:  "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPPBN1PP/R3KN1n w Q - 0 1",
 			want: []string{
 				"a2a3", "b2b3", "c2c3", "g2g3", "h2h3", "a2a4", "b2b4", "g2g4", "h2h4", "d7c8q", "d7c8r", "d7c8b", "d7c8n",
 				"f1e3", "f1g3", "e2c1", "e2g1", "e2c3", "e2g3", "e2d4", "e2f4", "d2c1", "d2c3", "d2e3", "d2b4", "d2f4", "d2a5",
@@ -240,7 +240,7 @@ func TestMoves(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b := tt.b
+			b := Must(board.FromFEN(tt.fen))
 
 			ms.Push()
 			defer ms.Pop()
@@ -267,7 +267,7 @@ func TestMoves(t *testing.T) {
 				uciStrs = append(uciStrs, m.String())
 			}
 
-			assert.ElementsMatch(t, uciStrs, tt.want)
+			assert.ElementsMatch(t, uciStrs, tt.want, "fen %s", tt.fen)
 		})
 	}
 }
@@ -276,27 +276,27 @@ func TestGenNoisy(t *testing.T) {
 	tests := []struct {
 		name string // description of this test case
 		// Named input parameters for target function.
-		b    *board.Board
+		fen  string
 		want []string
 	}{
 		{
 			name: "king captures",
-			b:    Must(board.FromFEN("8/8/7k/8/8/8/pK6/B7 w - - 0 1")),
+			fen:  "8/8/7k/8/8/8/pK6/B7 w - - 0 1",
 			want: []string{"b2a2"},
 		},
 		{
 			name: "knight captures",
-			b:    Must(board.FromFEN("8/6k1/8/8/8/p7/8/1N1K4 w - - 0 1")),
+			fen:  "8/6k1/8/8/8/p7/8/1N1K4 w - - 0 1",
 			want: []string{"b1a3"},
 		},
 		{
 			name: "bishop captures",
-			b:    Must(board.FromFEN("K7/1B6/p6k/8/8/8/8/8 w - - 0 1")),
+			fen:  "K7/1B6/p6k/8/8/8/8/8 w - - 0 1",
 			want: []string{"b7a6"},
 		},
 		{
 			name: "pawn captures",
-			b:    Must(board.FromFEN("6k1/8/8/8/8/p2PP2P/PP3P2/KB6 w - - 0 1")),
+			fen:  "6k1/8/8/8/8/p2PP2P/PP3P2/KB6 w - - 0 1",
 			want: []string{"b2a3"},
 		},
 	}
@@ -305,7 +305,7 @@ func TestGenNoisy(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b := tt.b
+			b := Must(board.FromFEN(tt.fen))
 
 			ms.Push()
 			defer ms.Pop()
@@ -331,7 +331,7 @@ func TestGenNoisy(t *testing.T) {
 				uciStrs = append(uciStrs, m.String())
 			}
 
-			assert.ElementsMatch(t, uciStrs, tt.want)
+			assert.ElementsMatch(t, uciStrs, tt.want, "fen %s", tt.fen)
 		})
 	}
 }
