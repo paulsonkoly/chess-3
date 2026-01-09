2536a5be0cbeb86e6d20ae154be185809702cfe9
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-01-09 09:06:57 +0000

    added mvvlva test
    exempt from sprt
    
    bench 12162929

---
+++ heur/mvvlva_test.go
@@ -0,0 +1,43 @@
+package heur_test
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/paulsonkoly/chess-3/board"
+	. "github.com/paulsonkoly/chess-3/chess"
+	"github.com/paulsonkoly/chess-3/heur"
+	"github.com/paulsonkoly/chess-3/move"
+	"github.com/paulsonkoly/chess-3/movegen"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMVVLVA(t *testing.T) {
+	b := Must(board.FromFEN("2n1k3/1P4b1/8/R2q3N/2P5/8/8/4K3 w - - 0 1"))
+
+	ms := move.NewStore()
+	ms.Push()
+	defer ms.Pop()
+
+	movegen.GenNoisy(ms, b)
+	moves := ms.Frame()
+
+	for i, m := range moves {
+		good := heur.SEE(b, m.Move, 0)
+		moves[i].Weight = heur.MVVLVA(b, m.Move, good)
+	}
+
+	slices.SortFunc(moves, func(a, b move.Weighted) int {
+		return int(b.Weight - a.Weight)
+	})
+
+	listMoves := make([]string, 0)
+	for _, m := range moves {
+		listMoves = append(listMoves, m.String())
+	}
+
+	assert.Equal(t, []string{
+		"b7c8q", "b7b8q", "b7c8r", "b7b8r", "b7c8b", "b7b8b", "b7c8n", "b7b8n",
+		"c4d5", "a5d5", "h5g7",
+	}, listMoves)
+}
