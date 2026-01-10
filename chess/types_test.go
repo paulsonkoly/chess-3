69f38cddefe87685c4b660822eecda98bc90d7ef
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-01-10 12:38:57 +0000

    added clarifying comment
    bench 11044545

--- chess/types_test.go
+++ chess/types_test.go
@@ -16,8 +16,11 @@ func TestScoreString(t *testing.T) {
 		{"0 score", 0, "cp 0"},
 		{"positive score", 73, "cp 73"},
 		{"negative score", -50, "cp -50"},
+		// 2 impossible edge cases:
+		//  - mate 0 means we were asked to run on a (legal) mate position
+		//  - mate -0 means the opponent is mated while it's our turn to move (illegal)
 		{"score for mating opponent", Inf, "mate 0"},
-		{"score for being mated", -Inf, "mate -0"},
+		{"score for being mated ", -Inf, "mate -0"},
 		{"score for mating opponent in 1 move", Inf - 1, "mate 1"},
 		{"score for being mated in 1 move", -Inf + 2, "mate -1"},
 		{"score for mating opponent in 2 moves", Inf - 3, "mate 2"},
