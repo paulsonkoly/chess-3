b257a0661618ecfed83874a31d915f92654f6638
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-01-13 13:36:41 +0000

    capthist
    bench 10201834

---
+++ heur/capthist.go
@@ -0,0 +1,36 @@
+package heur
+
+import (
+	. "github.com/paulsonkoly/chess-3/chess"
+)
+
+// CaptHist is capture history heuristics store.
+type CaptHist struct {
+	data [6][5][Squares]Score
+}
+
+// NewCaptHist creates a new history heuristics.
+func NewCaptHist() *CaptHist {
+	return &CaptHist{}
+}
+
+// Clear resets all entries to 0.
+func (c *CaptHist) Clear() {
+	c.data = [6][5][Squares]Score{}
+}
+
+// Add increments the capture history heuristics for the move by bonus.
+func (c *CaptHist) Add(moved, captured Piece, sq Square, bonus Score) {
+	clampedBonus := Clamp(bonus, -MaxCaptHistory, MaxCaptHistory)
+	moved -= Pawn // offset range ignoring NoPiece
+	captured -= Pawn
+	c.data[moved][captured][sq] +=
+		clampedBonus - Score(int(c.data[moved][captured][sq])*int(Abs(clampedBonus))/int(MaxCaptHistory))
+}
+
+// LookUp returns the history heuristics entry for the move.
+func (c *CaptHist) LookUp(moved, captured Piece, sq Square) Score {
+	moved -= Pawn // offset range ignoring NoPiece
+	captured -= Pawn
+	return c.data[moved][captured][sq]
+}
