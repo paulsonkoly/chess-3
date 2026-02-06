4022fc738099d9efd96c74d79139b2acf920ffe6
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-02-06 09:13:26 +0000

    Extract (#263)
    Added combined probability based sampling, filtering checks, checkmate score etc.
    
    exempt from sprt

--- tools/extract/extract.go
+++ tools/extract/extract.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
-	"math"
 	"math/rand/v2"
 	"os"
 	"runtime/pprof"
@@ -17,14 +16,18 @@ import (
 	"github.com/paulsonkoly/chess-3/chess"
 	"github.com/paulsonkoly/chess-3/eval"
 	"github.com/paulsonkoly/chess-3/move"
+	"github.com/paulsonkoly/chess-3/tools/extract/sampling"
 )
-
-const PhaseBucketCount = 24
 
 var (
 	dbFn          string
 	outFn         string
 	filterNoisy   bool
+	filterMate    bool
+	filterOutlier bool
+	filterInCheck bool
+	samplePhase   bool
+	sampleOutcome bool
 	samplePerGame int
 )
 
@@ -34,6 +37,11 @@ func main() {
 	flag.StringVar(&dbFn, "database", "database.db", "input database file name")
 	flag.StringVar(&outFn, "output", "extract.epd", "output epd file")
 	flag.BoolVar(&filterNoisy, "filterNoisy", true, "filter positions with bestmove being noisy")
+	flag.BoolVar(&filterMate, "filterMate", true, "filter positions with mate scores")
+	flag.BoolVar(&filterOutlier, "filterOutlier", true, "filter positions with eval mismatching wdl by margin")
+	flag.BoolVar(&filterInCheck, "filterInCheck", true, "filter in check positions")
+	flag.BoolVar(&samplePhase, "samplePhase", true, "sample positions for game phase")
+	flag.BoolVar(&sampleOutcome, "sampleOutcome", true, "sample positions for outcome")
 	flag.IntVar(&samplePerGame, "samplePerGame", 40, "number of maximum positions from a game; (-1) to disable")
 	flag.StringVar(&cpuProf, "cpuProf", "", "cpu profile (empty to disable)")
 
@@ -57,7 +65,7 @@ func main() {
 	}
 	defer db.Close()
 
-	entries, err := loadPositions(db)
+	entries, err := loadAndFilter(db)
 	if err != nil {
 		panic(err)
 	}
@@ -67,41 +75,62 @@ func main() {
 		os.Exit(1)
 	}
 
-	buckets, err := countPhases(entries)
-	if err != nil {
-		panic(err)
+	discretizers := []sampling.Discretizer{}
+	if samplePhase {
+		discretizers = append(discretizers,
+			sampling.NewFeature(eval.MaxPhase/3+1, func(d any) int {
+				if epdE, ok := d.(EPDEntry); ok {
+					var b board.Board
+					if err := board.ParseFEN(&b, epdE.fen); err != nil {
+						panic(err)
+					}
+
+					pieceCnt := 0
+					for pt := chess.Pawn; pt < chess.King; pt++ {
+						pieceCnt += b.Pieces[pt].Count() * eval.Phase[pt]
+					}
+					pieceCnt /= 3
+					pieceCnt = min(pieceCnt, eval.MaxPhase/3)
+					return pieceCnt
+				}
+				panic("interface conversion")
+			}),
+		)
 	}
 
-	fmt.Println(buckets)
-
-	k := math.MaxFloat64
-	for _, bucket := range buckets {
-		if bucket == 0 {
-			continue
-		}
-		rat := (float64(bucket) / float64(len(entries))) / (1.0 / float64(PhaseBucketCount))
-		if rat < k {
-			k = rat
-		}
+	if sampleOutcome {
+		discretizers = append(discretizers,
+			sampling.NewFeature(2, func(d any) int {
+				if epdE, ok := d.(EPDEntry); ok {
+					if epdE.wdl == 0 {
+						return 0 // draw
+					} else { // decisive
+						return 1
+					}
+				}
+				panic("interface conversion")
+			}),
+		)
 	}
 
-	keepProb := [PhaseBucketCount]float64{}
-	for ix, bucket := range buckets {
-		if bucket == 0 {
-			continue
-		}
-		dist := float64(bucket) / float64(len(entries))
-		keepProb[ix] = k * (1.0 / float64(PhaseBucketCount)) / dist
+	combined := sampling.NewCombined(discretizers...)
+	downScaled := sampling.NewScale(combined, int(float64(combined.Dim())*0.6))
+
+	counter := sampling.NewCounter(downScaled.Dim())
+	bar := progress.NewOptions(len(entries), progress.OptionSetDescription("counting features"))
+	for _, entry := range entries {
+		counter.Add(downScaled.Value(entry))
+		bar.Add(1)
 	}
+	bar.Close()
 
-	fmt.Println(keepProb)
-
-	if err := output(entries, keepProb[:]); err != nil {
+	sampler := sampling.NewUniformSampler(counter)
+	if err := output(entries, downScaled, sampler); err != nil {
 		panic(err)
 	}
 }
 
-func loadPositions(db *sql.DB) ([]EPDEntry, error) {
+func loadAndFilter(db *sql.DB) ([]EPDEntry, error) {
 
 	var entryCnt int
 	if err := db.QueryRow("select count(*) from positions").Scan(&entryCnt); err != nil {
@@ -121,7 +150,7 @@ func loadPositions(db *sql.DB) ([]EPDEntry, error) {
 	}
 	defer games.Close()
 
-	posStm, err := db.Prepare("select fen, best_move from positions where game_id=?")
+	posStm, err := db.Prepare("select fen, best_move, eval from positions where game_id=?")
 	if err != nil {
 		return nil, err
 	}
@@ -176,15 +205,34 @@ func loadGamePositions(posStm *sql.Stmt, gameId, wdl int, bar *progress.Progress
 
 	for positions.Next() {
 		var (
-			fen []byte
-			bm  move.Move
+			fen   []byte
+			bm    move.Move
+			score chess.Score
 		)
 
-		if err := positions.Scan(&fen, &bm); err != nil {
+		if err := positions.Scan(&fen, &bm, &score); err != nil {
 			return nil, err
 		}
 
 		bar.Add(1)
+
+		if err := board.ParseFEN(&b, fen); err != nil {
+			return nil, err
+		}
+
+		if filterOutlier {
+			if b.STM == chess.Black {
+				score = -score
+			}
+
+			if (score < -600 && wdl == WhiteWon) || (score > 600 && wdl == BlackWon) {
+				continue
+			}
+		}
+
+		if filterMate && score.IsMate() {
+			continue
+		}
 
 		if filterNoisy {
 			// filter noisy best moves
@@ -192,13 +240,13 @@ func loadGamePositions(posStm *sql.Stmt, gameId, wdl int, bar *progress.Progress
 				continue
 			}
 
-			if err := board.ParseFEN(&b, fen); err != nil {
-				return nil, err
-			}
-
 			if b.SquaresToPiece[bm.To()] != chess.NoPiece {
 				continue
 			}
+		}
+
+		if filterInCheck && b.InCheck(b.STM) {
+			continue
 		}
 
 		fenCopy := make([]byte, len(fen))
@@ -213,35 +261,7 @@ func loadGamePositions(posStm *sql.Stmt, gameId, wdl int, bar *progress.Progress
 	return gameEntries, nil
 }
 
-func countPhases(entries []EPDEntry) ([]int, error) {
-	buckets := [PhaseBucketCount]int{}
-	bar := progress.NewOptions(len(entries), progress.OptionSetDescription("sorting phases"))
-	defer bar.Close()
-
-	var b board.Board
-
-	for ix, entry := range entries {
-		if err := board.ParseFEN(&b, entry.fen); err != nil {
-			return nil, err
-		}
-
-		bucketIx := 0
-		for pt := chess.Pawn; pt < chess.King; pt++ {
-			bucketIx += b.Pieces[pt].Count() * eval.Phase[pt]
-		}
-
-		bucketIx = min(bucketIx, PhaseBucketCount-1)
-
-		entries[ix].bucketIx = bucketIx
-
-		buckets[bucketIx]++
-		bar.Add(1)
-	}
-
-	return buckets[:], nil
-}
-
-func output(entries []EPDEntry, keepProb []float64) error {
+func output(entries []EPDEntry, disc sampling.Discretizer, sampler sampling.Sampler) error {
 	outF, err := os.Create(outFn)
 	if err != nil {
 		return err
@@ -249,21 +269,17 @@ func output(entries []EPDEntry, keepProb []float64) error {
 	defer outF.Close()
 	outB := bufio.NewWriter(outF)
 
-	outBuckets := [PhaseBucketCount]int{}
-
 	bar := progress.NewOptions(len(entries), progress.OptionSetDescription("output"))
 	defer bar.Close()
 
 	for _, entry := range entries {
 		r := rand.Float64()
 
-		if r < keepProb[entry.bucketIx] {
+		if r < sampler.KeepProb(disc.Value(entry)) {
 			fmt.Fprintf(outB, "%s; %.1f\n", entry.fen, wdlToEPD(entry.wdl))
-			outBuckets[entry.bucketIx]++
 		}
 		bar.Add(1)
 	}
-	fmt.Println(outBuckets)
 
 	if err := outB.Flush(); err != nil {
 		return err
@@ -272,19 +288,24 @@ func output(entries []EPDEntry, keepProb []float64) error {
 	return nil
 }
 
+const (
+	Draw     = 0
+	WhiteWon = 1
+	BlackWon = 2
+)
+
 type EPDEntry struct {
-	fen      []byte
-	wdl      int
-	bucketIx int
+	fen []byte
+	wdl int
 }
 
 func wdlToEPD(n int) float64 {
 	switch n {
-	case 0:
+	case Draw:
 		return 0.5
-	case 1:
+	case WhiteWon:
 		return 1.0
-	case 2:
+	case BlackWon:
 		return 0.0
 	}
 	panic(fmt.Sprintf("unexpected wdl %d", n))
