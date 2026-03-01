9987f8e546ce229d45b29a4a5632f41086d4d1b9
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-03-01 17:57:37 +0000

    Pawn eval logic refactor (#279)
    Elo   | 8.49 +- 6.56 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=1MB
    LLR   | 2.95 (-2.94, 2.94) [-5.00, 0.00]
    Games | N: 4582 W: 1327 L: 1215 D: 2040
    Penta | [100, 497, 1005, 569, 120]
    https://paulsonkoly.pythonanywhere.com/test/586/
    
    bench 11851410

---
+++ eval/pawns.go
@@ -0,0 +1,81 @@
+package eval
+
+import (
+	"github.com/paulsonkoly/chess-3/board"
+
+	. "github.com/paulsonkoly/chess-3/chess"
+)
+
+// the player's side of the board with the extra 2 central squares included at
+// enemy side.
+var sideOfBoard = [2]BitBoard{0x00000018_ffffffff, 0xffffffff_18000000}
+
+type pawns struct {
+	pawns      [2]BitBoard
+	cover      [2]BitBoard
+	frontLine  [2]BitBoard
+	frontSpan  [2]BitBoard
+	neighbourF [2]BitBoard // neighbourF is files adjacent to files with pawns
+}
+
+func calcPawns(b *board.Board) *pawns {
+	pawns := pawns{}
+
+	ps := [...]BitBoard{b.Pieces[Pawn] & b.Colors[White], b.Pieces[Pawn] & b.Colors[Black]}
+	pawns.pawns = ps
+
+	pawns.frontSpan = [...]BitBoard{frontFill(ps[White], White) << 8, frontFill(ps[Black], Black) >> 8}
+	rearSpan := [...]BitBoard{frontFill(ps[White], Black) >> 8, frontFill(ps[Black], White) << 8}
+
+	wFiles := ps[White] | pawns.frontSpan[White] | rearSpan[White]
+	bFiles := ps[Black] | pawns.frontSpan[Black] | rearSpan[Black]
+	pawns.neighbourF = [...]BitBoard{
+		((wFiles & ^AFileBB) >> 1) | ((wFiles & ^HFileBB) << 1),
+		((bFiles & ^HFileBB) << 1) | ((bFiles & ^AFileBB) >> 1),
+	}
+
+	pawns.frontLine = [...]BitBoard{^rearSpan[White] & ps[White], ^rearSpan[Black] & ps[Black]}
+
+	pawns.cover = [...]BitBoard{
+		((pawns.frontSpan[White] & ^AFileBB) >> 1) | ((pawns.frontSpan[White] & ^HFileBB) << 1),
+		((pawns.frontSpan[Black] & ^HFileBB) << 1) | ((pawns.frontSpan[Black] & ^AFileBB) >> 1),
+	}
+
+	return &pawns
+}
+
+// holes are squares that cannot be protected by one of our pawns on our side of the board.
+func (p *pawns) holes(c Color) BitBoard {
+	return sideOfBoard[c] &^ p.cover[c]
+}
+
+// passers are pawns not stoppable by enemy pawns without them changing file.
+func (p *pawns) passers(c Color) BitBoard {
+	return p.frontLine[c] & ^(p.frontSpan[c.Flip()] | (p.cover[c.Flip()]))
+}
+
+// doubledPawns are pawns that have a friendly further advanced pawn on the same file.
+func (p *pawns) doubledPawns(c Color) BitBoard {
+	return (p.pawns[c]) &^ p.frontLine[c]
+}
+
+// isolatedPawns are pawns not having any friendly pawn on adjacent files.
+func (p *pawns) isolatedPawns(c Color) BitBoard {
+	return (p.pawns[c]) &^ p.neighbourF[c]
+}
+
+func frontFill(b BitBoard, color Color) BitBoard {
+	switch color {
+	case White:
+		b |= b << 8
+		b |= b << 16
+		b |= b << 32
+
+	case Black:
+		b |= b >> 8
+		b |= b >> 16
+		b |= b >> 32
+	}
+
+	return b
+}
---
+++ eval/pawns_test.go
@@ -0,0 +1,157 @@
+package eval
+
+import (
+	"testing"
+
+	"github.com/paulsonkoly/chess-3/board"
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/paulsonkoly/chess-3/chess"
+)
+
+func TestHoles(t *testing.T) {
+	tests := [...]struct {
+		name  string
+		fen   string
+		color Color
+		want  BitBoard
+	}{
+		{
+			"startpos white",
+			"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+			White,
+			BitBoardFromSquares(A1, B1, C1, D1, E1, F1, G1, H1, A2, B2, C2, D2, E2, F2, G2, H2),
+		},
+		{
+			"startpos black",
+			"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+			Black,
+			BitBoardFromSquares(A7, B7, C7, D7, E7, F7, G7, H7, A8, B8, C8, D8, E8, F8, G8, H8),
+		},
+		{
+			"complicated pawn structure with white",
+			"7k/2ppp3/1p3p2/p5pP/2P3Pp/1P1P1P2/P3P3/4K3 w - - 0 1",
+			White,
+			BitBoardFromSquares(
+				A1, B1, C1, D1, E1, F1, G1, H1,
+				A2, B2, C2, D2, E2, F2, G2, H2,
+				A3, C3, E3, G3, H3,
+				H4,
+			),
+		},
+		{
+			"complicated pawn structure with black",
+			"7k/2ppp3/1p3p2/p5pP/2P3Pp/1P1P1P2/P3P3/4K3 w - - 0 1",
+			Black,
+			BitBoardFromSquares(
+				H5,
+				A6, G6, H6,
+				A7, B7, C7, D7, E7, F7, G7, H7,
+				A8, B8, C8, D8, E8, F8, G8, H8,
+			),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := Must(board.FromFEN(tt.fen))
+			pawns := calcPawns(b)
+
+			assert.Equal(t, tt.want, pawns.holes(tt.color), "fen %s color %v", tt.fen, tt.color)
+		})
+	}
+}
+
+func TestPassers(t *testing.T) {
+	tests := [...]struct {
+		name  string
+		fen   string
+		color Color
+		want  BitBoard
+	}{
+		{"startpos white", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", White, 0},
+		{"startpos black", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Black, 0},
+		{"empty board", "4k3/8/8/8/8/8/8/4K3 w - - 0 1", White, 0},
+		{"single pawn", "4k3/1p6/8/8/8/8/8/4K3 w - - 0 1", Black, BitBoardFromSquares(B7)},
+		{"connected pawns, not passers", "4k3/1p6/8/8/8/8/PP6/4K3 w - - 0 1", White, 0},
+		{"blocked pawn on edge", "4k3/8/8/p7/P7/8/8/4K3 w - - 0 1", White, 0},
+		{"blocked pawn in the middle", "4k3/8/8/3p4/3P4/8/8/4K3 w - - 0 1", White, 0},
+		{"pawn on same file already passed", "4k3/8/8/4P3/4p3/8/8/4K3 w - - 0 1", White, BitBoardFromSquares(E5)},
+		{"pawn blocked by both enemy frontline & cover", "4k3/1p6/p7/8/P7/8/8/4K3 w - - 0 1", White, 0},
+		{"doubled pawns", "4k3/8/8/8/8/1P6/1P6/4K3 w - - 0 1", White, BitBoardFromSquares(B3)},
+		{"pawn one step from promotion", "4k3/2P5/8/8/8/8/8/4K3 w - - 0 1", White, BitBoardFromSquares(C7)},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := Must(board.FromFEN(tt.fen))
+			pawns := calcPawns(b)
+
+			assert.Equal(t, tt.want, pawns.passers(tt.color), "fen %s color %v", tt.fen, tt.color)
+		})
+	}
+}
+
+func TestDoubledPawns(t *testing.T) {
+	tests := [...]struct {
+		name  string
+		fen   string
+		color Color
+		want  BitBoard
+	}{
+		{"startpos white", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", White, 0},
+		{"startpos black", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Black, 0},
+		{"empty board", "4k3/8/8/8/8/8/8/4K3 w - - 0 1", White, 0},
+		{"single pawn", "4k3/8/8/8/8/8/1P6/4K3 w - - 0 1", White, 0},
+		{"two pawns stacked vertically", "4k3/8/8/8/8/1P6/1P6/4K3 w - - 0 1", White, BitBoardFromSquares(B2)},
+		{"three pawns stacked vertically", "4k3/8/8/8/1P6/1P6/1P6/4K3 w - - 0 1", White, BitBoardFromSquares(B2, B3)},
+		{"two pawns on same file with gap", "4k3/8/8/8/1P6/8/1P6/4K3 w - - 0 1", White, BitBoardFromSquares(B2)},
+		{
+			"two pawns on same file with enemy pawn in gap",
+			"4k3/8/8/8/1P6/1p6/1P6/4K3 w - - 0 1",
+			White,
+			BitBoardFromSquares(B2),
+		},
+		{"connected pawns", "4k3/8/8/8/8/2P5/1P6/4K3 w - - 0 1", White, 0},
+		{"doubled on a file", "4k3/8/8/8/P7/P7/8/4K3 w - - 0 1", White, BitBoardFromSquares(A3)},
+		{"doubled on h file", "4k3/8/7P/7P/8/8/8/4K3 w - - 0 1", White, BitBoardFromSquares(H5)},
+		{"two sets of doubled pawns", "4k3/8/8/P7/P7/1P6/1P6/4K3 w - - 0 1", White, BitBoardFromSquares(B2, A4)},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := Must(board.FromFEN(tt.fen))
+			pawns := calcPawns(b)
+
+			assert.Equal(t, tt.want, pawns.doubledPawns(tt.color), "fen %s color %v", tt.fen, tt.color)
+		})
+	}
+}
+
+func TestIsolatedPawns(t *testing.T) {
+	tests := [...]struct {
+		name  string
+		fen   string
+		color Color
+		want  BitBoard
+	}{
+		{"startpos white", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", White, 0},
+		{"startpos black", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Black, 0},
+		{"empty board", "4k3/8/8/8/8/8/8/4K3 w - - 0 1", White, 0},
+		{"single isolated pawn on a-file", "4k3/8/8/8/8/P7/8/4K3 w - - 0 1", White, BitBoardFromSquares(A3)},
+		{"single isolated pawn on h-file", "4k3/8/8/8/8/8/7P/4K3 w - - 0 1", White, BitBoardFromSquares(H2)},
+		{"single isolated pawn on e-file", "4k3/8/8/8/8/4P3/8/4K3 w - - 0 1", White, BitBoardFromSquares(E3)},
+		{"connected pawns", "4k3/8/8/4P3/8/8/5P2/4K3 w - - 0 1", White, 0},
+		{"pawn connected to enemy pawn", "4k3/8/8/4Pp2/8/8/8/4K3 w - - 0 1", White, BitBoardFromSquares(E5)},
+		{"pawn on edge supported by chain", "4k3/8/8/8/P7/1P6/8/4K3 w - - 0 1", White, 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := Must(board.FromFEN(tt.fen))
+			pawns := calcPawns(b)
+
+			assert.Equal(t, tt.want, pawns.isolatedPawns(tt.color), "fen %s color %v", tt.fen, tt.color)
+		})
+	}
+}
