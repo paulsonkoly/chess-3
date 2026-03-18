7c31d99e8e7a56ba42fed56cacc5d6964791ff3c
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-03-18 13:45:04 +0000

    replace type with color
    bench 16200679

--- heur/cont.go
+++ heur/cont.go
@@ -4,19 +4,10 @@ import (
 	. "github.com/paulsonkoly/chess-3/chess"
 )
 
-type ContinuationType byte
-
-const (
-	FollowUp = ContinuationType(iota)
-	Counter
-
-	Continuations
-)
-
 // Continuation is the heuristics table indexed with color, old move piece type
 // & to square, new move piece type and to square.
 type Continuation struct {
-	data [Continuations][Colors][6][Squares][6][Squares]Score
+	data [Colors][6][Squares][Colors][6][Squares]Score
 }
 
 // NewContinuation creates a continuation history table.
@@ -26,18 +17,18 @@ func NewContinuation() *Continuation {
 
 // Clear clears the continuation history table.
 func (c *Continuation) Clear() {
-	c.data = [Continuations][Colors][6][Squares][6][Squares]Score{}
+	c.data = [Colors][6][Squares][Colors][6][Squares]Score{}
 }
 
 // Add increments the continuation history heuristics for the move by bonus.
-func (c *Continuation) Add(t ContinuationType, stm Color, ptHist Piece, toHist Square, pt Piece, to Square, bonus Score) {
-	entry := &c.data[t][stm][ptHist-1][toHist][pt-1][to]
+func (c *Continuation) Add(cHist Color, ptHist Piece, toHist Square, stm Color, pt Piece, to Square, bonus Score) {
+	entry := &c.data[cHist][ptHist-1][toHist][stm][pt-1][to]
 
 	clampedBonus := Clamp(bonus, -MaxHistory, MaxHistory)
 	*entry += clampedBonus - Score(int(*entry)*int(Abs(clampedBonus))/int(MaxHistory))
 }
 
 // LookUp returns the continuation history heuristics entry for the move.
-func (c *Continuation) LookUp(t ContinuationType, stm Color, ptHist Piece, toHist Square, pt Piece, to Square) Score {
-	return c.data[t][stm][ptHist-1][toHist][pt-1][to]
+func (c *Continuation) LookUp(cHist Color, ptHist Piece, toHist Square, stm Color, pt Piece, to Square) Score {
+	return c.data[cHist][ptHist-1][toHist][stm][pt-1][to]
 }
