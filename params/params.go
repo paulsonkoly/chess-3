cf17691bc64a1669aff38ca604434e29845c2b6f
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-03-24 12:42:23 +0000

    See pruning (#324)
    Elo   | 9.54 +- 5.86 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=1MB
    LLR   | 2.94 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 6048 W: 1733 L: 1567 D: 2748
    Penta | [133, 696, 1244, 774, 177]
    https://paulsonkoly.pythonanywhere.com/test/647/
    
    bench 13522698

--- params/params.go
+++ params/params.go
@@ -9,19 +9,22 @@ package params
 // between the two files must be maintained.
 
 const (
-	NMPDiffFactor    = 51
-	NMPDepthLimit    = 1
-	NMPInit          = 4
-	RFPDepthLimit    = 8
-	RFPScoreFactor   = 102
-	WindowSize       = 44
-	LMRStart         = 2
-	StandPatDelta    = 113
-	HistBonusMul     = 20
-	HistBonusLin     = 15
-	HistAdjRange     = 8
-	HistAdjReduction = 7
-	IIRDepthLimit    = 5
+	NMPDiffFactor         = 51
+	NMPDepthLimit         = 1
+	NMPInit               = 4
+	RFPDepthLimit         = 8
+	RFPScoreFactor        = 102
+	WindowSize            = 44
+	LMRStart              = 2
+	StandPatDelta         = 113
+	HistBonusMul          = 20
+	HistBonusLin          = 15
+	HistAdjRange          = 8
+	HistAdjReduction      = 7
+	IIRDepthLimit         = 5
+	SEEPruningDepthLimit  = 7
+	SEEPruningQuietMargin = -84
+	SEEPruningNoisyMargin = -35
 )
 
 // UCIOptions returns the uci options string for tunable parameters in an spsa
