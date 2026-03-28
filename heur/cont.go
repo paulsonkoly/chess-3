ced52bf4f8c7c1eae93ec2016598f9184b22c473
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-03-28 17:19:08 +0000

    continuation lookup optmimizations
    bench 13716780

--- heur/cont.go
+++ heur/cont.go
@@ -4,13 +4,15 @@ import (
 	. "github.com/paulsonkoly/chess-3/chess"
 )
 
+const contStoreSize = int(Colors) * 6 * int(Squares) * int(Colors) * 6 * int(Squares)
+
 // Continuation is the continuation history heuristics table.
 //
 // Indexed by oldSTM, oldPiece, oldTo, currSTM, currPiece, currTo where old
 // refers to a move that happened some plies ago, curr refers to the current
 // move.
 type Continuation struct {
-	data [Colors][6][Squares][Colors][6][Squares]Score
+	data [contStoreSize]Score
 }
 
 // NewContinuation creates a continuation history table.
@@ -20,7 +22,7 @@ func NewContinuation() *Continuation {
 
 // Clear clears the continuation history table.
 func (c *Continuation) Clear() {
-	c.data = [Colors][6][Squares][Colors][6][Squares]Score{}
+	c.data = [contStoreSize]Score{}
 }
 
 // Add increments the continuation history heuristics for the move by bonus.
@@ -33,7 +35,7 @@ func (c *Continuation) Add(
 	currTo Square,
 	bonus Score,
 ) {
-	entry := &c.data[oldSTM][oldPiece-1][oldTo][currSTM][currPiece-1][currTo]
+	entry := &c.data[ix(oldSTM, oldPiece, oldTo, currSTM, currPiece, currTo)]
 
 	clampedBonus := Clamp(bonus, -MaxHistory, MaxHistory)
 	*entry += clampedBonus - Score(int(*entry)*int(Abs(clampedBonus))/int(MaxHistory))
@@ -48,5 +50,16 @@ func (c *Continuation) LookUp(
 	currPiece Piece,
 	currTo Square,
 ) Score {
-	return c.data[oldSTM][oldPiece-1][oldTo][currSTM][currPiece-1][currTo]
+	return c.data[ix(oldSTM, oldPiece, oldTo, currSTM, currPiece, currTo)]
+}
+
+func ix(oldSTM Color, oldPiece Piece, oldTo Square, currSTM Color, currPiece Piece, currTo Square) int {
+	const (
+		X0 = 6 * int(Colors) * int(Squares) * int(Colors) * int(Squares)
+		X1 = int(Colors) * int(Squares) * int(Colors) * int(Squares)
+		X2 = int(Squares) * int(Colors) * int(Squares)
+		X3 = int(Colors) * int(Squares)
+		X4 = int(Squares)
+	)
+	return int(oldPiece-1)*X0 + int(currPiece-1)*X1 + int(oldSTM)*X2 + int(oldTo)*X3 + int(currSTM)*X4 + int(currTo)
 }
