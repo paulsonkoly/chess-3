56420c2d895fb0313ef2950fa18bcd548dd18c3d
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-04-08 10:37:37 +0100

    suggestions
    bench 14289660

--- tools/tuner/eval/eval.go
+++ tools/tuner/eval/eval.go
@@ -8,12 +8,14 @@ import (
 
 type CoeffSet eval.CoeffSet[float64]
 
+// ColorRelative is a float64 version of eval.
 type ColorRelative eval.Eval[float64]
 
 func New() *ColorRelative {
 	return (*ColorRelative)(eval.New[float64]())
 }
 
+// Score is the evaluation score relative to color not stm.
 func (cr *ColorRelative) Score(b *board.Board, coeffs *CoeffSet) float64 {
 	score := ((*eval.Eval[float64])(cr)).Score(b, (*eval.CoeffSet[float64])(coeffs))
 	if b.STM == Black {
