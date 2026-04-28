6b553635ec0724d43e63f9efdb59e83859f3aec3
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-04-28 20:37:40 +0100

    psqt only
    bench 13144020

--- eval/eval.go
+++ eval/eval.go
@@ -19,65 +19,63 @@ const (
 )
 
 type Eval[T ScoreType] struct {
-	sp            [Colors][Phases]T
-	scaleFactor   [Colors]T
-	kingAttacks   [Colors]T
-	attacks       [Colors][Pieces]BitBoard
-	cover         [Colors]BitBoard
-	pawns         [Colors]Pawns
-	kings         [Colors]Kings
-	matFuncs      [evalIDs]evalFunc[T]
-	pawnCache     []PawnCache
-	pawnKingCache []PawnKingCache
-	materialCache []MaterialCache[T]
-}
-
-type Pawns struct {
-	cover      BitBoard
-	frontline  BitBoard
-	backmost   BitBoard
-	frontspan  BitBoard
-	neighbourF BitBoard
-}
-
-type Kings struct {
-	nb BitBoard
-	sq Square
+	sp [Colors][Phases]T
 }
 
 func New[T ScoreType]() *Eval[T] {
-	e := &Eval[T]{
-		pawnCache:     make([]PawnCache, PawnCacheSize),
-		pawnKingCache: make([]PawnKingCache, PawnKingCacheSize),
-		materialCache: make([]MaterialCache[T], materialCacheSize),
-	}
-	e.matFuncs[evalInsufficientID] = evalInsufficient[T]
-	e.matFuncs[evalKNBvKID] = evalKNBvK[T]
-	e.matFuncs[evalOCBID] = evalOCB[T]
-	e.matFuncs[evalOCBKnightsID] = evalOCBKnights[T]
-	e.matFuncs[evalOCBRooksID] = evalOCBRooks[T]
-	e.matFuncs[evalKNvKPID] = evalKNvKP[T]
-	e.matFuncs[evalKBvKPID] = evalKBvKP[T]
-	e.matFuncs[evalKRvKNID] = evalKRvKN[T]
-	e.matFuncs[evalKRvKBID] = evalKRvKB[T]
-	e.matFuncs[evalPositionalID] = evalPositional[T]
-	return e
+	return &Eval[T]{}
 }
 
 func (e *Eval[T]) Clear() {
-	for i := range e.pawnCache {
-		e.pawnCache[i] = PawnCache{}
-	}
-
-	for i := range e.pawnKingCache {
-		e.pawnKingCache[i] = PawnKingCache{}
-	}
-
-	for i := range e.materialCache {
-		e.materialCache[i] = MaterialCache[T]{}
-	}
 }
 
+// Blend is game phase.
+var Blend = [...]int{0, 0, 1, 1, 2, 4, 0}
+
+const (
+	// MaxBlend is the sum of pieces on the starting position each piece counted as
+	// the corresponding Phase value.
+	MaxBlend = 24
+)
+
 func (e *Eval[T]) Score(b *board.Board, c *CoeffSet[T]) T {
-	return e.material(b, c)
+	e.sp = [2][2]T{}
+
+	var phase int
+	for color := range Colors {
+		for pType := Pawn; pType <= King; pType++ {
+			for pieces := b.Colors[color] & b.Pieces[pType]; pieces != 0; pieces &= pieces - 1 {
+				if pType != King {
+					sq := pieces.LowestSet()
+
+					if color == White {
+						sq ^= 56 // upside down
+					}
+
+					ix := pType - 1
+
+					e.sp[color][MG] += c.PSqT[2*ix][sq]
+					e.sp[color][EG] += c.PSqT[2*ix+1][sq]
+
+					phase += Blend[pType]
+				}
+				e.sp[color][MG] += c.PieceValues[MG][pType]
+				e.sp[color][EG] += c.PieceValues[EG][pType]
+			}
+		}
+	}
+
+	mgScore := e.sp[b.STM][MG] - e.sp[b.STM.Flip()][MG]
+	egScore := e.sp[b.STM][EG] - e.sp[b.STM.Flip()][EG]
+
+	mgPhase := min(phase, MaxBlend)
+	egPhase := MaxBlend - mgPhase
+
+	if _, ok := (any(mgScore)).(Score); ok {
+		v := int(mgScore)*mgPhase + int(egScore)*egPhase
+		return T(v / MaxBlend)
+	}
+
+	v := mgScore*T(mgPhase) + egScore*T(egPhase)
+	return v / MaxBlend
 }
