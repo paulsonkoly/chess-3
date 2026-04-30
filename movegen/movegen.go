4da2096e1d189f337e15643bfcb4191dc6f95173
Paul Sonkoly <sonkoly.pal@gmail.com>
2026-04-30 11:29:42 +0100

    movegen refactor
    - using toMsk more consistently in prep for evasions
    - removed gen struct
    
    bench 15654623

--- movegen/movegen.go
+++ movegen/movegen.go
@@ -8,15 +8,11 @@ import (
 	. "github.com/paulsonkoly/chess-3/chess"
 )
 
-type generator struct {
-	self, them, occ BitBoard
-}
-
-func (g generator) kingMoves(ms *move.Store, b *board.Board, fromMsk, toMsk BitBoard) {
-	if piece := g.self & b.Pieces[King] & fromMsk; piece != 0 {
+func kingMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	if piece := b.Colors[b.STM] & b.Pieces[King]; piece != 0 {
 		from := piece.LowestSet()
 
-		tSqrs := attacks.KingMoves(from) & ^g.self & toMsk
+		tSqrs := attacks.KingMoves(from) & toMsk
 		for ; tSqrs != 0; tSqrs &= tSqrs - 1 {
 			to := tSqrs.LowestSet()
 			ms.Alloc(move.From(from) | move.To(to))
@@ -24,12 +20,11 @@ func (g generator) kingMoves(ms *move.Store, b *board.Board, fromMsk, toMsk BitB
 	}
 }
 
-func (g generator) knightMoves(ms *move.Store, b *board.Board, fromMsk, toMsk BitBoard) {
-	knights := g.self & b.Pieces[Knight] & fromMsk
-	for ; knights != 0; knights &= knights - 1 {
+func knightMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	for knights := b.Colors[b.STM] & b.Pieces[Knight]; knights != 0; knights &= knights - 1 {
 		from := knights.LowestSet()
 
-		tSqrs := attacks.KnightMoves(from) & ^g.self & toMsk
+		tSqrs := attacks.KnightMoves(from) & toMsk
 		for ; tSqrs != 0; tSqrs &= tSqrs - 1 {
 			to := tSqrs.LowestSet()
 			ms.Alloc(move.From(from) | move.To(to))
@@ -37,12 +32,12 @@ func (g generator) knightMoves(ms *move.Store, b *board.Board, fromMsk, toMsk Bi
 	}
 }
 
-func (g generator) bishopMoves(ms *move.Store, b *board.Board, fromMsk, toMsk BitBoard) {
-	bishops := g.self & b.Pieces[Bishop] & fromMsk
-	for ; bishops != 0; bishops &= bishops - 1 {
+func bishopMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	occ := b.Colors[White] | b.Colors[Black]
+	for bishops := b.Colors[b.STM] & b.Pieces[Bishop]; bishops != 0; bishops &= bishops - 1 {
 		from := bishops.LowestSet()
 
-		tSqrs := attacks.BishopMoves(from, g.occ) & ^g.self & toMsk
+		tSqrs := attacks.BishopMoves(from, occ) & toMsk
 		for ; tSqrs != 0; tSqrs &= tSqrs - 1 {
 			to := tSqrs.LowestSet()
 			ms.Alloc(move.From(from) | move.To(to))
@@ -50,12 +45,12 @@ func (g generator) bishopMoves(ms *move.Store, b *board.Board, fromMsk, toMsk Bi
 	}
 }
 
-func (g generator) rookMoves(ms *move.Store, b *board.Board, fromMsk, toMsk BitBoard) {
-	rooks := g.self & b.Pieces[Rook] & fromMsk
-	for ; rooks != 0; rooks &= rooks - 1 {
+func rookMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	occ := b.Colors[White] | b.Colors[Black]
+	for rooks := b.Colors[b.STM] & b.Pieces[Rook]; rooks != 0; rooks &= rooks - 1 {
 		from := rooks.LowestSet()
 
-		tSqrs := attacks.RookMoves(from, g.occ) & ^g.self & toMsk
+		tSqrs := attacks.RookMoves(from, occ) & toMsk
 		for ; tSqrs != 0; tSqrs &= tSqrs - 1 {
 			to := tSqrs.LowestSet()
 			ms.Alloc(move.From(from) | move.To(to))
@@ -63,12 +58,12 @@ func (g generator) rookMoves(ms *move.Store, b *board.Board, fromMsk, toMsk BitB
 	}
 }
 
-func (g generator) queenMoves(ms *move.Store, b *board.Board, fromMsk, toMsk BitBoard) {
-	queens := g.self & b.Pieces[Queen] & fromMsk
-	for ; queens != 0; queens &= queens - 1 {
+func queenMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	occ := b.Colors[White] | b.Colors[Black]
+	for queens := b.Colors[b.STM] & b.Pieces[Queen]; queens != 0; queens &= queens - 1 {
 		from := queens.LowestSet()
 
-		tSqrs := (attacks.BishopMoves(from, g.occ) | attacks.RookMoves(from, g.occ)) & ^g.self & toMsk
+		tSqrs := (attacks.BishopMoves(from, occ) | attacks.RookMoves(from, occ)) & toMsk
 		for ; tSqrs != 0; tSqrs &= tSqrs - 1 {
 			to := tSqrs.LowestSet()
 			ms.Alloc(move.From(from) | move.To(to))
@@ -78,28 +73,30 @@ func (g generator) queenMoves(ms *move.Store, b *board.Board, fromMsk, toMsk Bit
 
 var shifts = [2]Square{8, -8}
 
-func (g generator) singlePushMoves(ms *move.Store, b *board.Board, fromMsk BitBoard) {
-	occ1 := (g.occ >> 8) << (b.STM << 4)
-	pushable := g.self & b.Pieces[Pawn] & ^occ1
+// Note: toMsk should ensure that there is no push onto occupied square.
+func singlePushMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	shifted := attacks.PawnSinglePushMoves(toMsk, b.STM.Flip())
+	pushable := b.Colors[b.STM] & b.Pieces[Pawn] & shifted
 	shift := shifts[b.STM]
 	mySeventhRank := RankBB(SeventhRank.FromPerspectiveOf(b.STM))
 
 	// single pawn pushes (no promotions)
-	for pawns := pushable & fromMsk & ^mySeventhRank; pawns != 0; pawns &= pawns - 1 {
+	for pawns := pushable & ^mySeventhRank; pawns != 0; pawns &= pawns - 1 {
 		from := pawns.LowestSet()
 
 		ms.Alloc(move.From(from) | move.To(from+shift))
 	}
 }
 
-func (g generator) promoPushMoves(ms *move.Store, b *board.Board, fromMsk BitBoard) {
-	occ1 := ((g.occ >> 8) << (b.STM << 4)) | ((g.occ << 8) >> (b.STM.Flip() << 4))
-	pushable := g.self & b.Pieces[Pawn] & ^occ1
+// Note: toMsk should ensure that there is no push onto occupied square.
+func promoPushMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	shifted := attacks.PawnSinglePushMoves(toMsk, b.STM.Flip())
+	pushable := b.Colors[b.STM] & b.Pieces[Pawn] & shifted
 	shift := shifts[b.STM]
 	mySeventhRank := RankBB(SeventhRank.FromPerspectiveOf(b.STM))
 
 	// promotions pushes
-	for pawns := pushable & fromMsk & mySeventhRank; pawns != 0; pawns &= pawns - 1 {
+	for pawns := pushable & mySeventhRank; pawns != 0; pawns &= pawns - 1 {
 		from := pawns.LowestSet()
 		for promo := Queen; promo > Pawn; promo-- {
 			ms.Alloc(move.From(from) | move.To(from+shift) | move.Promo(promo))
@@ -107,41 +104,34 @@ func (g generator) promoPushMoves(ms *move.Store, b *board.Board, fromMsk BitBoa
 	}
 }
 
-func (g generator) doublePushMoves(ms *move.Store, b *board.Board, fromMsk BitBoard) {
-	occ1 := (g.occ >> 8) << (b.STM << 4)
-	occ2 := (g.occ >> 16) << (b.STM << 5)
-	pushable := g.self & b.Pieces[Pawn] & ^occ1
+// Note: toMsk should ensure that there is no push onto occupied square. We
+// ensure that the square between source and target squares is unoccupied.
+func doublePushMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	occ := b.Colors[White] | b.Colors[Black]
+	shifted1 := attacks.PawnSinglePushMoves(toMsk, b.STM.Flip())
+	shifted2 := attacks.PawnSinglePushMoves(shifted1 & ^occ, b.STM.Flip())
+	pushable := b.Colors[b.STM] & b.Pieces[Pawn] & shifted2
 	shift := shifts[b.STM]
 	mySecondRank := RankBB(SecondRank.FromPerspectiveOf(b.STM))
 
 	// double pawn pushes
-	for pawns := pushable & ^occ2 & fromMsk & mySecondRank; pawns != 0; pawns &= pawns - 1 {
+	for pawns := pushable & mySecondRank; pawns != 0; pawns &= pawns - 1 {
 		from := pawns.LowestSet()
 		ms.Alloc(move.From(from) | move.To(from+2*shift))
 	}
 }
 
-func (g generator) pawnCaptureMoves(ms *move.Store, b *board.Board) {
-	var (
-		occ1l, occ1r BitBoard
-	)
-
-	if b.STM == White {
-		occ1l = (g.them &^ HFileBB) >> 7
-		occ1r = (g.them &^ AFileBB) >> 9
-	} else {
-		occ1l = (g.them &^ AFileBB) << 7
-		occ1r = (g.them &^ HFileBB) << 9
-	}
-
+// Note: toMsk should ensure that there is no capture onto a square not occupied by NSTM.
+func pawnCaptureMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	shifted := attacks.PawnCaptureMoves(toMsk, b.STM.Flip())
 	mySeventhRank := RankBB(SeventhRank.FromPerspectiveOf(b.STM))
+	pawns := b.Colors[b.STM] & b.Pieces[Pawn] & ^mySeventhRank & shifted
 
-	pawns := g.self & b.Pieces[Pawn] & ^mySeventhRank & (occ1l | occ1r)
 	for ; pawns != 0; pawns &= pawns - 1 {
 		from := pawns.LowestSet()
 
 		pawn := pawns & -pawns
-		tSqrs := attacks.PawnCaptureMoves(pawn, b.STM) & g.them
+		tSqrs := attacks.PawnCaptureMoves(pawn, b.STM) & toMsk
 		for tSqr := BitBoard(0); tSqrs != 0; tSqrs ^= tSqr {
 			tSqr = tSqrs & -tSqrs
 			to := tSqr.LowestSet()
@@ -150,25 +140,18 @@ func (g generator) pawnCaptureMoves(ms *move.Store, b *board.Board) {
 	}
 }
 
-func (g generator) pawnCapturePromoMoves(ms *move.Store, b *board.Board) {
-	var (
-		occ1l, occ1r BitBoard
-	)
-
-	if b.STM == White {
-		occ1l = (g.them &^ HFileBB) >> 7
-		occ1r = (g.them &^ AFileBB) >> 9
-	} else {
-		occ1l = (g.them &^ AFileBB) << 7
-		occ1r = (g.them &^ HFileBB) << 9
-	}
+// Note: toMsk should ensure that there is no capture onto a square not occupied by NSTM.
+func pawnCapturePromoMoves(ms *move.Store, b *board.Board, toMsk BitBoard) {
+	shifted := attacks.PawnCaptureMoves(toMsk, b.STM.Flip())
 	mySeventhRank := RankBB(SeventhRank.FromPerspectiveOf(b.STM))
+	pawns := b.Colors[b.STM] & b.Pieces[Pawn] & mySeventhRank & shifted
+
 	// pawn captures with promotions
-	for pawns := g.self & b.Pieces[Pawn] & mySeventhRank & (occ1l | occ1r); pawns != 0; pawns &= pawns - 1 {
+	for ; pawns != 0; pawns &= pawns - 1 {
 		from := pawns.LowestSet()
 
 		pawn := pawns & -pawns
-		tSqrs := attacks.PawnCaptureMoves(pawn, b.STM) & g.them
+		tSqrs := attacks.PawnCaptureMoves(pawn, b.STM) & toMsk
 		for tSqr := BitBoard(0); tSqrs != 0; tSqrs ^= tSqr {
 			tSqr = tSqrs & -tSqrs
 			to := tSqr.LowestSet()
@@ -180,20 +163,20 @@ func (g generator) pawnCapturePromoMoves(ms *move.Store, b *board.Board) {
 	}
 }
 
-func (g generator) enPassant(ms *move.Store, b *board.Board) {
+func enPassant(ms *move.Store, b *board.Board) {
 	if b.EnPassant == 0 {
 		return
 	}
 
 	// en-passant
 	ep := attacks.PawnCaptureMoves(1<<b.EnPassant, b.STM.Flip())
-	for pawns := ep & g.self & b.Pieces[Pawn]; pawns != 0; pawns &= pawns - 1 {
+	for pawns := ep & b.Colors[b.STM] & b.Pieces[Pawn]; pawns != 0; pawns &= pawns - 1 {
 		from := pawns.LowestSet()
 		ms.Alloc(move.From(from) | move.To(b.EnPassant))
 	}
 }
 
-func (g generator) shortCastle(ms *move.Store, b *board.Board, rChkMsk BitBoard) {
+func shortCastle(ms *move.Store, b *board.Board) {
 	var castleMask BitBoard
 	switch b.STM {
 	case White:
@@ -201,20 +184,23 @@ func (g generator) shortCastle(ms *move.Store, b *board.Board, rChkMsk BitBoard)
 	case Black:
 		castleMask = BitBoardFromSquares(E8, F8, G8)
 	}
-	if b.Castles&Castle(b.STM, Short) != 0 && g.occ&castleMask == g.self&b.Pieces[King] {
+
+	occ := b.Colors[White] | b.Colors[Black]
+	king := b.Colors[b.STM] & b.Pieces[King]
+	if b.Castles&Castle(b.STM, Short) != 0 && occ&castleMask == king {
 		// this isn't quite the right condition, we would need to properly
 		// calculate if the rook gives check this condition is simple, and would
 		// suffice most of the time
-		if castleMask&rChkMsk != 0 {
-			if !b.IsAttacked(b.STM.Flip(), g.occ, castleMask) {
-				from := (g.self & b.Pieces[King]).LowestSet()
+		if castleMask != 0 {
+			if !b.IsAttacked(b.STM.Flip(), occ, castleMask) {
+				from := king.LowestSet()
 				ms.Alloc(move.From(from) | move.To(from+2))
 			}
 		}
 	}
 }
 
-func (g generator) longCastle(ms *move.Store, b *board.Board, rChkMsk BitBoard) {
+func longCastle(ms *move.Store, b *board.Board) {
 	var castleMask BitBoard
 	switch b.STM {
 	case White:
@@ -222,10 +208,13 @@ func (g generator) longCastle(ms *move.Store, b *board.Board, rChkMsk BitBoard) 
 	case Black:
 		castleMask = BitBoardFromSquares(E8, D8, C8)
 	}
-	if b.Castles&Castle(b.STM, Long) != 0 && g.occ&(castleMask>>1) == 0 {
-		if castleMask&rChkMsk != 0 {
-			if !b.IsAttacked(b.STM.Flip(), g.occ, castleMask) {
-				from := (g.self & b.Pieces[King]).LowestSet()
+
+	occ := b.Colors[White] | b.Colors[Black]
+	king := b.Colors[b.STM] & b.Pieces[King]
+	if b.Castles&Castle(b.STM, Long) != 0 && occ&(castleMask>>1) == 0 {
+		if castleMask != 0 {
+			if !b.IsAttacked(b.STM.Flip(), occ, castleMask) {
+				from := king.LowestSet()
 				ms.Alloc(move.From(from) | move.To(from-2))
 			}
 		}
@@ -235,52 +224,34 @@ func (g generator) longCastle(ms *move.Store, b *board.Board, rChkMsk BitBoard) 
 // Noisy generates all noisy (captures/promotions) pseudo-legal moves in the
 // position.
 func Noisy(ms *move.Store, b *board.Board) {
-
-	self := b.Colors[b.STM]
 	them := b.Colors[b.STM.Flip()]
 	occ := b.Colors[White] | b.Colors[Black]
 
-	gen := generator{
-		self: self,
-		them: them,
-		occ:  occ,
-	}
+	kingMoves(ms, b, them)
+	knightMoves(ms, b, them)
+	bishopMoves(ms, b, them)
+	rookMoves(ms, b, them)
+	queenMoves(ms, b, them)
 
-	gen.kingMoves(ms, b, Full, them)
-	gen.knightMoves(ms, b, Full, them)
-	gen.bishopMoves(ms, b, Full, them)
-	gen.rookMoves(ms, b, Full, them)
-	gen.queenMoves(ms, b, Full, them)
-
-	gen.promoPushMoves(ms, b, Full)
-	gen.pawnCaptureMoves(ms, b)
-	gen.pawnCapturePromoMoves(ms, b)
-	gen.enPassant(ms, b)
-
+	promoPushMoves(ms, b, ^occ)
+	pawnCaptureMoves(ms, b, them)
+	pawnCapturePromoMoves(ms, b, them)
+	enPassant(ms, b)
 }
 
 // Quiet generates all pseudo-legal moves not generated by Noisy.
 func Quiet(ms *move.Store, b *board.Board) {
-
-	self := b.Colors[b.STM]
-	them := b.Colors[b.STM.Flip()]
 	occ := b.Colors[White] | b.Colors[Black]
 
-	gen := generator{
-		self: self,
-		them: them,
-		occ:  occ,
-	}
+	kingMoves(ms, b, ^occ)
+	knightMoves(ms, b, ^occ)
+	bishopMoves(ms, b, ^occ)
+	rookMoves(ms, b, ^occ)
+	queenMoves(ms, b, ^occ)
 
-	gen.kingMoves(ms, b, Full, ^them)
-	gen.knightMoves(ms, b, Full, ^them)
-	gen.bishopMoves(ms, b, Full, ^them)
-	gen.rookMoves(ms, b, Full, ^them)
-	gen.queenMoves(ms, b, Full, ^them)
+	singlePushMoves(ms, b, ^occ)
+	doublePushMoves(ms, b, ^occ)
 
-	gen.singlePushMoves(ms, b, Full)
-	gen.doublePushMoves(ms, b, Full)
-
-	gen.shortCastle(ms, b, Full)
-	gen.longCastle(ms, b, Full)
+	shortCastle(ms, b)
+	longCastle(ms, b)
 }
